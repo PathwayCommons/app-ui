eb78d80ddd52e4d4868df6e9a09d7f92e2dba565
Dylan Fong <d2fong@uwaterloo.ca>
2017-09-28 13:54:53 -0400

    Add react analytics

--- src/client/router.js
+++ src/client/router.js
@@ -1,18 +1,31 @@
 
-const {BrowserRouter, Route, Switch} = require('react-router-dom');
+const {BrowserRouter, Route} = require('react-router-dom');
 const h = require('react-hyperscript');
+const ReactGA = require('react-ga');
 const _ = require('lodash');
-const Button = require('material-ui').Button;
+
 const Features = require('./features');
 
 const Entry = () => h('div', 'entry');
-const Search = () => h(Button, {className: 'search'}, 'search');
+const Search = () => h('div', {className: 'search'}, 'search');
 const View = () => h('div', 'view');
-const Err = () => h('div', 'err');
+
+
+ReactGA.initialize('UA-43341809-7');
+const Analytics = (props) => {
+  ReactGA.set({ page: props.location.pathname + props.location.search });
+  ReactGA.pageview(props.location.pathname + props.location.search);
+  return null;
+};
+
 
 module.exports = () => {
   return h(BrowserRouter, [
-    h(Switch, [
+    h('div', [
+      {
+        path: '*',
+        render: props => h(Analytics, props)
+      },
       {
         path: '/',
         render: props => h(Entry, props)
@@ -28,10 +41,6 @@ module.exports = () => {
       {
         path: '/paint',
         render: props => h(Features.Paint, props)
-      },
-      {
-        path: '*',
-        render: props => h(Err, props)
       }
     ].map( spec => h(Route, _.assign({ exact: true }, spec)) ))
   ]);

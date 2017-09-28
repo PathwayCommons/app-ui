6f6ebb9bf179ab6b00aad1c6d084f708eeac0992
Dylan Fong <d2fong@uwaterloo.ca>
2017-09-28 12:05:40 -0400

    Remove material ui

---
+++ src/client/features/paint/index.js
@@ -0,0 +1,11 @@
+const React = require('react');
+const h = require('react-hyperscript');
+
+class Paint extends React.Component {
+
+  render() {
+    return h('div', 'paint');
+  }
+}
+
+module.exports = Paint;

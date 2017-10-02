7223b90da0a7e4bee2cc3086345afeef26171588
Dylan Fong <d2fong@uwaterloo.ca>
2017-10-02 16:58:53 -0400

    bare bones entry page

--- src/client/features/entry/index.js
+++ src/client/features/entry/index.js
@@ -1,11 +1,54 @@
 const React = require('react');
 const h = require('react-hyperscript');
+const queryString = require('query-string');
+const _ = require('lodash');
 
 const Icon = require('../../common/components').Icon;
-const SearchBar = require('../../common/components').SearchBar;
 
+
+// requires react router props
 class Entry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: _.assign({q: '', gt: 3, lt: 250}, this.props.query)
+    };
+  }
+
+  onSearchValueChange(e) {
+    // if the user presses enter, submit the query
+    if (e.which && e.which ===  13) {
+      this.submitSearchQuery(e);
+    } else {
+      const newQueryState = _.assign({}, this.state.query);
+      newQueryState.q = e.target.value;
+      this.setState({query: newQueryState});
+    }
+  }
+
+  submitSearchQuery() {
+    const props = this.props;
+    const state = this.state;
+
+    const query = state.query;
+    const uriRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+
+    if (query.q.match(uriRegex)) {
+      props.history.push({
+        pathname: '/view',
+        search: queryString.stringify({uri: state.query.q}),
+        state: {}
+      });
+    } else {
+      props.history.push({
+        pathname: '/search',
+        search: queryString.stringify(query),
+        state: {}
+      });
+    }
+  }
   render() {
+    const state = this.state;
     return h('div.entry', [
       h('div.entry-header', [
         h('a.entry-pc-link', {
@@ -18,10 +61,12 @@ class Entry extends React.Component {
           h('h5.entry-pc-description', 'Search pathways from public databases')
         ]),
         h('div.entry-search', [
-          h(SearchBar, {
-            className: 'entry-searchbar',
+          h('input.entry-searchbar', {
+            type: 'text',
             placeholder: 'Enter pathway name or gene names',
-            query: {}
+            value: state.query.q,
+            onChange: e => this.onSearchValueChange(e),
+            onKeyPress: e => this.onSearchValueChange(e)
           }),
           h('div.entry-searchicon', [
             h('a', [

4a149eca61e5cdc6236ac2c0484b5dca76a80196
Dylan Fong <d2fong@uwaterloo.ca>
2017-10-06 15:57:12 -0400

    Refactor search logic, uses async await instead of promises, deletes queryProcessor.js

--- src/client/services/pathway-commons/search/index.js
+++ src/client/services/pathway-commons/search/index.js
@@ -6,14 +6,6 @@ const removeSpaces = (token) => {
   return token.replace(/(\s+)/g, '\\$1');
 };
 
-const processQueryString = (queryString) => {
-  return getHGNCData('hgncSymbols.txt')
-    .then(hgncSymbols => {
-      const processedQuery = processPhrase(queryString, hgncSymbols);
-      return '(name:' + removeSpaces(queryString) + ') OR (' + 'name:*' + removeSpaces(queryString) + '*) OR (' + processedQuery.join(' AND ') + ')';
-    });
-};
-
 const processPhrase = (phrase, collection) => {
   const sourceList = [
     'uniprot',
@@ -35,32 +27,50 @@ const processPhrase = (phrase, collection) => {
     });
 };
 
+const processQueryString = async (queryString) => {
+  return getHGNCData('hgncSymbols.txt')
+    .then(hgncSymbols => {
+      const processedQuery = processPhrase(queryString, hgncSymbols);
+
+      // return three query candidates to search, first query is fastest, last query slowest
+      return [
+        '(name:' + removeSpaces(queryString) + ') OR (' + 'name:*' + removeSpaces(queryString) + '*) OR (' + processedQuery.join(' AND ') + ')',
+        '(' + processedQuery + ')',
+        queryString
+      ];
+    });
+};
+
 // needs query object with the following values:
 //  - q: string to search
 //  - lt: max graph size result returned
 //  - gt: min graph size result returned
 //  - type: the type of object to get (usually 'Pathway')
 
-const querySearch = (query) => {
-  return processQueryString(query.q.trim())
-    .then(queryValue => {
-      return search()
+const querySearch = async (query) => {
+  const processedQueries = await processQueryString(query.q.trim());
+
+  for (let pq of processedQueries) {
+    const searchResult = await search()
       .query(query)
-      .q(queryValue)
+      .q(pq)
       .format('json')
-      .fetch()
-      .then(searchResult => {
-        const minResultSize = query.gt || 250;
-        const maxResultSize = query.lt || 3;
+      .fetch();
 
-        const filteredResults = searchResult.searchHit.filter(hit => {
-          const resultSize = hit.numParticipants ? hit.numParticipants : 0;
-          return minResultSize < resultSize && resultSize < maxResultSize;
-        });
+    if (searchResult.searchHit.length > 0) {
+      const minResultSize = query.gt || 250;
+      const maxResultSize = query.lt || 3;
 
-        return filteredResults;
+      const filteredResults = searchResult.searchHit.filter(hit => {
+        const resultSize = hit.numParticipants ? hit.numParticipants : 0;
+        return minResultSize < resultSize && resultSize < maxResultSize;
       });
-    });
+
+      return filteredResults;
+    }
+  }
+
+  return [];
 };
 
 module.exports = querySearch;

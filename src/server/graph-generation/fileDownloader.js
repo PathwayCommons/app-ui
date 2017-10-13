a33c5903f2bc238bced46b080828adc48bc9359b
Harsh Mistry <hnmistry@uwaterloo.ca>
2017-10-13 11:24:03 -0400

    Added cyJson.js and filedownloader.js
    - Added ability to download SBGN and Biopax files from pc2
    - Added ability to download pathway metadata (Title, description, datasource)
    - Added ability automate the procces by just providing an uri to cytoscapeJson.js

---
+++ src/server/graph-generation/fileDownloader.js
@@ -0,0 +1,63 @@
+/**
+    Pathway Commons Central Data Cache
+
+    File Downloader
+    fileDownloader.js
+
+    Purpose : Downloads SBGN, BioPax, Title, and Metadata files from PC2
+
+    Requires : Valid URI's
+
+    Effects : Downloads files asynchronously
+
+    Note : Script may take time to download from pc2, due to uptime issues
+
+    @author Harsh Mistry
+    @version 1.1 2017/10/10
+**/
+
+var fetch = require('node-fetch');
+
+//Traverse Options
+const traverseOptions = {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+};
+
+//Traverse Options
+const getOptions = {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'text',
+  }
+};
+
+//Returns the request file from pathway commons
+//Requires uri and format to be valid xml files
+function getPC2(uri, format) {
+  //Construct query url
+  var uri = encodeURIComponent(uri);
+  var url = 'https://www.pathwaycommons.org/pc2/get?format=' + format + '&uri=' + uri + '&user=pathwaycommons-js-lib%3Apathways-search';
+  return fetch(url, getOptions).then(response => response.text());
+}
+
+//Queries PC2 using traverse
+//Returns metadata for the pathway
+//Requires a valid query and uri
+function traversePC2(uri, path) {
+  //Construct query url
+  uri = encodeURIComponent(uri);
+  path = encodeURIComponent(path);
+  var url = 'https://www.pathwaycommons.org/pc2/traverse?format=json&uri=' + uri + '&path=' + path;
+  return fetch(url, traverseOptions).then(response => response.json());
+}
+
+//Export Functions
+module.exports = {
+  getPC2: getPC2,
+  traversePC2: traversePC2
+}
+

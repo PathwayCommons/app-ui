5d6418c57f102873f875214b95d9937a74a5fbb5
Dylan Fong <d2fong@uwaterloo.ca>
2017-10-16 13:59:14 -0400

    Basic paint view

--- src/client/features/paint/index.js
+++ src/client/features/paint/index.js
@@ -2,27 +2,30 @@ const React = require('react');
 const h = require('react-hyperscript');
 const queryString = require('query-string');
 
+const cytoscape = require('cytoscape');
+const sbgn2Json = require('sbgnml-to-cytoscape');
+const sbgnStylesheet = require('cytoscape-sbgn-stylesheet');
+
 const PathwayCommonsService = require('../../services/').PathwayCommonsService;
 
 class Paint extends React.Component {
   constructor(props) {
     super(props);
+
     const query = queryString.parse(props.location.search);
+    const cy = cytoscape({
+      style: sbgnStylesheet(cytoscape),
+      minZoom: 0.16,
+      maxZoom: 4,
+      headless: true
+    });
 
     this.state = {
       query: query,
-      cy: {},
-      sbgnText: {},
+      cy: cy,
       name: '',
       datasource: ''
     };
-
-    PathwayCommonsService.query(query.uri, 'SBGN')
-      .then(text => {
-        this.setState({
-          sbgnText: text
-        });
-      });
 
     PathwayCommonsService.query(query.uri, 'json', 'Named/displayName')
       .then(response => {
@@ -39,10 +42,29 @@ class Paint extends React.Component {
       });
   }
 
+  componentWillUnmount() {
+    this.state.cy.destroy();
+  }
+
+  componentDidMount() {
+    const state = this.state;
+    const container = document.getElementById('cy-container');
+    state.cy.mount(container);
+
+    PathwayCommonsService.query(state.query.uri, 'SBGN')
+    .then(text => {
+      const sbgnJson = sbgn2Json(text);
+      state.cy.remove('*');
+      state.cy.add(sbgnJson);
+    });
+  }
+
   render() {
     return h('div.paint', [
       h('div.paint-menu'),
-      h('div.paint-graph')
+      h('div.paint-graph', [
+        h(`div.#cy-container`, {style: {width: '100%', height: '100%'}})
+      ])
     ]);
   }
 }

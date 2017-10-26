373489ec12946d3130fc7b91fda7c8a99be26ae9
Geoff Elder <gaelder@uwaterloo.ca>
2017-10-26 11:09:10 -0400

    Refactored promise error handling in Route files

--- src/server/routes/databaseRoutes.js
+++ src/server/routes/databaseRoutes.js
@@ -41,79 +41,69 @@ var returnRouter = function () {
   //Get Layout
   router.get('/Get', function (req, res) {
     //Get the requested layout
-    try {
-      connPromise.then((connection) => {
-        var graph = accessDB.getGraph(
-          req.query.uri,
-          req.query.version,
-          connection
-        );
-        var layout = accessDB.getLayout(
-          req.query.uri,
-          req.query.version,
-          connection
-        );
 
-        return Promise.all([graph, layout]);
-      }).catch((e) => {
-        throw e;
-      }).then(([graph, layout]) => {
-        // Optimal case - everything works.
-        if (graph && layout) {
-          res.json({ graph: graph.graph, layout: layout.positions });
-          // Graph exists but layout not found. Return null to tell front end to a default load precomputed layout
-        } else if (graph) {
-          res.json({ graph: graph.graph, layout: null });
-          // Layout exists without a graph. This should be truly impossible and should throw an error.
-        } else if (layout) {
-          throw new Error('Error: Case not handled. But this really shouldn\'t happen');
-          // No graph or layout. Lazy load everything from pathway commons 
-        } else {
-          lazyLoad.queryPC(req.query.uri, req.query.version).then((result) => {
-            if (result) {
-              res.json({ graph: result, layout: null });
+    connPromise.then((connection) => {
+      var graph = accessDB.getGraph(
+        req.query.uri,
+        req.query.version,
+        connection
+      );
+      var layout = accessDB.getLayout(
+        req.query.uri,
+        req.query.version,
+        connection
+      );
 
-            } else {
-              res.json(null);
-            }
-          });
-        }
+      return Promise.all([graph, layout]);
+    }).then(([graph, layout]) => {
+      // Optimal case - everything works.
+      if (graph && layout) {
+        res.json({ graph: graph.graph, layout: layout.positions });
+        // Graph exists but layout not found. Return null to tell front end to a default load precomputed layout
+      } else if (graph) {
+        res.json({ graph: graph.graph, layout: null });
+        // Layout exists without a graph. This should be truly impossible and should throw an error.
+      } else if (layout) {
+        throw new Error('Error: Case not handled. But this really shouldn\'t happen');
+        // No graph or layout. Lazy load everything from pathway commons 
+      } else {
+        lazyLoad.queryPC(req.query.uri, req.query.version).then((result) => {
+          if (result) {
+            res.json({ graph: result, layout: null });
 
-      }).catch((e) => {
-        res.json(e);
-        //throw e;
-      });
-    } catch (e) {
+          } else {
+            res.json(null);
+          }
+        });
+      }
+
+    }).catch((e) => {
+      console.log(e);
       res.json('ERROR : Layout Request Failed!');
-    }
+    });
+
   });
 
   router.get('/getEditKey', function (req, res) {
-
-    try {
-      connPromise.then((connection) => {
-        if (auth.checkUser(req)) {
-          accessDB.getGraphID(
-            req.query.uri,
-            req.query.version,
-            connection,
-            function (result) {
-              if (result) {
-                res.json(result);
-              } else {
-                res.json('ERROR: No edit key could be found');
-              }
-            });
-        } else {
-          res.json('ERROR: Non-authenticated user');
-        }
-      }).catch((e) => {
-        res.json('ERROR: Edit Key Request Failed');
-      });
-    }
-    catch (e) {
+    connPromise.then((connection) => {
+      if (auth.checkUser(req)) {
+        accessDB.getGraphID(
+          req.query.uri,
+          req.query.version,
+          connection,
+          function (result) {
+            if (result) {
+              res.json(result);
+            } else {
+              res.json('ERROR: No edit key could be found');
+            }
+          });
+      } else {
+        res.json('ERROR: Non-authenticated user');
+      }
+    }).catch((e) => {
       res.json('ERROR: Edit Key Request Failed');
-    }
+    });
   });
 
   router.get('/checkEditKey', function (req, res) {
@@ -122,20 +112,18 @@ var returnRouter = function () {
       return;
     }
 
-    try {
-      connPromise.then((connection) => {
-        accessDB.getGraphID(
-          req.query.uri,
-          req.query.version,
-          connection,
-          function (result) { res.json(result === req.query.key); }
-        );
-      }).catch((err) => {
-        res.json('ERROR : Edit Priviliges Check Failed');
-      });
-    } catch (e) {
+
+    connPromise.then((connection) => {
+      accessDB.getGraphID(
+        req.query.uri,
+        req.query.version,
+        connection,
+        function (result) { res.json(result === req.query.key); }
+      );
+    }).catch((err) => {
       res.json('ERROR : Edit Priviliges Check Failed');
-    }
+    });
+
 
   });
 
@@ -145,26 +133,23 @@ var returnRouter = function () {
       return;
     }
     //Get the requested layout
-    try {
-      connPromise.then((connection) => {
-        if (auth.checkUser(req)) {
-          accessDB.saveLayout(req.body.uri,
-            req.body.layout,
-            req.body.version,
-            connection,
-            function () { res.json('Success: Update Applied!'); });
-        }
-        else {
-          res.json('ERROR : You Do Not Have Valid Permissions!');
-        }
-      }).catch((e) => {
-        res.json('ERROR: Layout Update Failed!');
-      });
-    }
-    catch (e) {
-      res.json('ERROR : Layout Update Failed!');
-      throw e;
-    }
+
+    connPromise.then((connection) => {
+      if (auth.checkUser(req)) {
+        accessDB.saveLayout(req.body.uri,
+          req.body.layout,
+          req.body.version,
+          connection,
+          function () { res.json('Success: Update Applied!'); });
+      }
+      else {
+        res.json('ERROR : You Do Not Have Valid Permissions!');
+      }
+    }).catch((e) => {
+      res.json('ERROR: Layout Update Failed!');
+    });
+
+
   });
 
   //Return Confirmation

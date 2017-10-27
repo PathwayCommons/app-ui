906f0eb573ce4522c2c342a571c5a2d6c77a529e
Dylan Fong <d2fong@uwaterloo.ca>
2017-10-27 11:44:30 -0400

    Remove templating functionality from server

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -1,53 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const queryString = require('query-string');
-
-const graphgen = require('../graph-generation/cytoscapeJson.js');
-
-
-router.post('/paint', function (req, res, next) {
-  res.render('paint', { paintData: JSON.stringify(req.body) });
-});
-
 
 /* GET home page.
 All URLS not specified earlier in server/index.js (e.g. REST URLs) get handled by the React UI */
-
-
-
 router.get('*', function (req, res, next) {
-  const url = req.originalUrl;
-  console.log(url);
-  if (url.substring(0, 6) === '/view?') {
-    const query = queryString.parse(url.slice(5));
-    if (query.uri) {
-      graphgen.getPathwayLevelMetadata(query.uri).then(md => {
-        console.log(md);
-
-        var title = md.title,
-          ds = md.dataSource,
-          comments_arr = md.comments,
-          comments = '';
-
-        for (var i = 0; i < comments_arr.length; i++) {
-          comments += comments_arr[i] + (i === comments_arr.length - 1 ? '' : '\n');
-        }
-
-        console.log(title);
-        console.log(ds);
-        console.log(comments);
-
-        if (!title) title = 'Pathway Commons';
-        if (!ds) ds = 'Unknown datasource';
-        if (!comments) comments = 'An interaction from Pathway Commons';
-
-        const displayTitle = title + ' | ' + ds;
-        res.render('index', { title: displayTitle, desc: comments });
-      });
-    }
-  } else {
-    res.render('index', { title: 'Pathway Commons', desc: 'Search our database of pathways and interactions!' });
-  }
+  res.render('index.html');
 });
 
 module.exports = router;

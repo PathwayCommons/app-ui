f42b60c0045a1c5e1c100127a69ec1b5f8d159b2
Geoff Elder <gaelder@uwaterloo.ca>
2017-11-01 11:12:55 -0400

    Added ability to set database config through env vraiables

--- src/server/database/config.js
+++ src/server/database/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  databaseName : 'metadataTestMk2',
-  ip: '127.0.0.1',
-  port: '28015'
+  databaseName : process.env.DATABASE_NAME || 'metadataTestMk2',
+  ip: process.env.DATABASE_IP || '127.0.0.1',
+  port: process.env.DATABASE_PORT ||'28015'
 };

f9830b7a4995eeb324af153291a699c9eafb0e33
Geoff Elder <gaelder@uwaterloo.ca>
2017-11-06 10:48:55 -0500

    Removed edit key check code from the backend api and implemented the API to save diffs and handle simultaneous editors

--- src/server/routes/databaseRoutes/sockets.js
+++ src/server/routes/databaseRoutes/sockets.js
@@ -3,31 +3,66 @@ const controller = require('./controller');
 const express = require('express');
 const btoa = require('btoa');
 const router = express.Router();
+const qs = require('querystring');
 
 
 function getLayout(io, socket, ioPackage) {
+  //console.log(Object.keys(socket));
+  //console.log(Object.keys(socket.client));
+  //console.log(socket.client);
+
   controller.getLayout(ioPackage.uri, ioPackage.version).then((package) => {
     socket.emit('layoutPackage', btoa(package));
   });
 }
 
 function submitLayout(io, socket, ioPackage) {
-  controller.submitLayout(ioPackage.uri, ioPackage.version, ioPackage.layout, ioPackage.key)
+  controller.submitLayout(ioPackage.uri, ioPackage.version, ioPackage.layout)
     .then((package) => {
       io.emit('updated', package);
     });
 }
 
-var returnRouter = function (io) {
+function submitDiff(io, socket, ioPackage) {
+  controller.submitDiff(ioPackage.uri, ioPackage.version, ioPackage.layout, socket.id)
+  .then((package)=>{
+    io.emit('updated', package);
+  });
+}
+
+let returnRouter = function (io) {
   io.on('connection', function (socket) {
     //Get Layout
     socket.on('getLayout', function (ioPackage) {
+      console.log(socket.id);
+      // Add socketID/userID to User table.
+      // Store graphID 
       getLayout(io, socket, ioPackage);
     });
 
     //Submit Layout
     socket.on('submitLayout', function (ioPackage) {
       submitLayout(io, socket, ioPackage);
+    });
+
+    socket.on('submitDiff', function(ioPackage){
+      submitDiff(io,socket,ioPackage);
+    });
+
+    socket.on('disconnect', function () {
+      console.log(socket.id);
+      let userURL = socket.handshake.headers.referer;
+
+      let editParams = userURL.match(/edit\?(.*)/);
+
+      if (editParams){
+
+        let params = qs.parse(editParams[1]);
+        let pcID = params.uri;
+        let releaseID = params.releaseID || 'latest';
+
+        controller.endSession(pcID, releaseID, socket.id);
+      }
     });
   });
 

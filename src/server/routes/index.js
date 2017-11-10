93fbded18caa01111802acc50c0d9cd535705daf
Geoff Elder <gaelder@uwaterloo.ca>
2017-11-10 15:36:37 -0500

    Reordered router definitions so that routes function properly

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -3,17 +3,19 @@ const router = express.Router();
 
 const restRoutes = require('./databaseRoutes/rest');
 
+router.post('/submit-layout', restRoutes.submitLayout);
+router.post('/submit-graoh', restRoutes.submitGraph);
+router.post('/submit-diff', restRoutes.submitDiff);
+router.get('/get-graph-and-layout', restRoutes.getGraphAndLayout);
+router.get('/disconnect', restRoutes.disconnect);
+
 /* GET home page.
 All URLS not specified earlier in server/index.js (e.g. REST URLs) get handled by the React UI */
 router.get('*', function (req, res, next) {
   res.render('index.html');
 });
 
-router.post('/submit-layout', restRoutes.submitLayout);
-router.post('/submit-graoh', restRoutes.submitGraph);
-router.post('/submit-diff', restRoutes.submitDiff);
-router.get('/get-graph-and-layout', restRoutes.getGraphAndLayout);
-router.get('/disconnect', restRoutes.disconnect);
+
 
 
 module.exports = router;

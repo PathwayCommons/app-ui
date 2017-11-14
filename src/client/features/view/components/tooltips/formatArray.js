6aa34a9cf7f70d539c9d5e1d3d80008336a4602d
Harsh Mistry <hnmistry@uwaterloo.ca>
2017-11-14 13:35:26 -0500

    Updated collectionToBottom

--- src/client/features/view/components/tooltips/formatArray.js
+++ src/client/features/view/components/tooltips/formatArray.js
@@ -10,9 +10,11 @@ function pushToBottom(data, field) {
 //Push a collection of elements to the bottom of the list
 //Takes in a list of id's and pushes all matching items to the bottom of the list
 //Requires a valid list of ids
-function collectionToBottom(data, idList) {
-  idList.forEach(field => data = pushToBottom(data, field));
-  return data;
+function collectionToBottom(data, criteriaList) {
+  const matched = data.filter(pair => criteriaList.includes(pair[0]));
+  const notMatched = _.difference(data, matched);
+  return notMatched.concat(_.sortBy(matched, pair => criteriaList.indexOf(pair[0])));
+  //return notMatched.concat(matchedSorted);
 }
 
 module.exports = {

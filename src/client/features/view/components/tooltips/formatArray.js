13d36d1ca6871fb3d8cbe79c180e860dd70b4add
Harsh Mistry <hnmistry@uwaterloo.ca>
2017-11-14 14:11:39 -0500

    Removed Push to Bottom

--- src/client/features/view/components/tooltips/formatArray.js
+++ src/client/features/view/components/tooltips/formatArray.js
@@ -1,12 +1,5 @@
 const _ = require('lodash');
 
-//Push a metadata field to the bottom of the list
-function pushToBottom(data, field) {
-  let withoutField = _.filter(data, pair => pair[0] !== field);
-  let withField = _.filter(data, pair => pair[0] === field);
-  return _.concat(withoutField, withField);
-}
-
 //Push a collection of elements to the bottom of the list
 //Takes in a list of id's and pushes all matching items to the bottom of the list
 //Requires a valid list of ids
@@ -18,6 +11,5 @@ function collectionToBottom(data, criteriaList) {
 }
 
 module.exports = {
-  pushToBottom,
   collectionToBottom
 };

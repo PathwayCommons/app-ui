137cdc52830c1b20a685f44d3125a2d9250bb2c9
Jonah Dlin <jdlin@uwaterloo.ca>
2017-11-15 14:39:59 -0500

    Added more links and changed around some text in downloads menu.

--- src/client/features/view/components/sidebar/menus/components/downloadDescriptions.js
+++ src/client/features/view/components/sidebar/menus/components/downloadDescriptions.js
@@ -9,10 +9,14 @@ function DescPng() {
 function DescGmt() {
   return (
     h('span', [
-      'Gene Matrix Transposed format. The gene Database of named gene sets (UniProt) useful for performing enrichment analysis using ',
       h('a', {
-        'target': '_blank',
-        'href': 'http://software.broadinstitute.org/gsea/index.jsp'
+        target: '_blank',
+        href: 'http://software.broadinstitute.org/cancer/software/gsea/wiki/index.php/Data_formats#GMT:_Gene_Matrix_Transposed_file_format_.28.2A.gmt.29'
+      }, 'Gene Matrix Transposed'),
+      ' format. The gene database of named gene sets (UniProt) useful for performing enrichment analysis using ',
+      h('a', {
+        target: '_blank',
+        href: 'http://software.broadinstitute.org/gsea/index.jsp'
       }, 'Gene Set Enrichment Analysis (GSEA)')
     ])
   );
@@ -22,13 +26,13 @@ function DescSif() {
   return (
     h('span', [
       h('a', {
-        'target': '_blank',
-        'href': 'http://wiki.cytoscape.org/Cytoscape_User_Manual/Network_Formats'
+        target: '_blank',
+        href: 'http://wiki.cytoscape.org/Cytoscape_User_Manual/Network_Formats'
       }, 'Simple interaction format'),
       ' (SIF) is a list of interaction pairs useful for viewing, styling, and editing using ',
       h('a', {
-        'target': '_blank',
-        'href': 'http://cytoscape.org/'
+        target: '_blank',
+        href: 'http://cytoscape.org/'
       }, 'Cytoscape desktop software'),
       ', and for analysis with graph algorithms.'
     ])
@@ -40,15 +44,15 @@ function DescTxt() {
     h('span', [
       'Similar to the ',
       h('a', {
-        'target': '_blank',
-        'href': 'http://wiki.cytoscape.org/Cytoscape_User_Manual/Network_Formats'
+        target: '_blank',
+        href: 'http://wiki.cytoscape.org/Cytoscape_User_Manual/Network_Formats'
       }, 'SIF'),
-      ' output, but contains extra information on interactions and a second table with information on nodes. Well-suited to a ',
+      ' output, but contains extra information on entities and interactions. See the SIF section on the ',
       h('a', {
-        'target': '_blank',
-        'href': 'http://cytoscape.org/'
-      }, 'Cytoscape - Attribute Table'),
-      ' import.'
+        target: '_blank',
+        href: 'http://www.pathwaycommons.org/pc2/formats'
+      }, 'PC2 formats page'),
+      ' for more details.'
     ])
   );
 }
@@ -57,13 +61,13 @@ function DescBiopax() {
   return (
     h('span', [
       h('a', {
-        'target': '_blank',
-        'href': 'http://www.biopax.org/'
+        target: '_blank',
+        href: 'http://www.biopax.org/'
       }, 'Biological Pathways Exchange (BioPAX)'),
       ' format includes all details of the biological network stored in Pathway Commons. It is recommended that this format be interpreted using tools like ',
       h('a', {
-        'target': '_blank',
-        'href': 'http://biopax.github.io/Paxtools/'
+        target: '_blank',
+        href: 'http://biopax.github.io/Paxtools/'
       }, 'Paxtools'),
       ' or Jena SPARQL.'
     ])
@@ -74,8 +78,8 @@ function DescJsonld() {
   return (
     h('span', [
       h('a', {
-        'target': '_blank',
-        'href': 'https://json-ld.org/'
+        target: '_blank',
+        href: 'https://json-ld.org/'
       }, 'JSON-LD'),
       ' is a human-readable linked format. This format is ideal for programming environments, REST web services, and unstructured databses.'
     ])
@@ -86,8 +90,8 @@ function DescSbgn() {
   return (
     h('span', [
       h('a', {
-        'target': '_blank',
-        'href': 'http://www.biopax.org/'
+        target: '_blank',
+        href: 'http://www.biopax.org/'
       }, 'Systems Biology Graphical Notation (SBGN)'),
       ' is a standard visual notation for biological networks. This download provides an XML in SBGN markup language (SBGN-ML).'
     ])

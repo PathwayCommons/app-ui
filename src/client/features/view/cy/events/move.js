3adb67c9b8bddc9c3a5d10e926c3852ff72718d4
Dylan Fong <d2fong@uwaterloo.ca>
2017-11-21 16:25:34 -0500

    Delete view/cy, view/tooltip -- these are in the common folder

--- src/client/features/view/cy/events/move.js
+++
@@ -1,9 +0,0 @@
-const CDC = require('../../../../services').CDC;
-
-const bindMove = (uri, version, cy) => {
-  cy.on('free', 'node', function(evt) {
-    CDC.submitNodeChange(uri, version, evt.target.id(), evt.target.position());
-  });
-};
-
-module.exports = bindMove;

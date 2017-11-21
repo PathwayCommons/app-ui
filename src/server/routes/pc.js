de216814130b44f45e7bc4667827bb543977df67
Geoff Elder <gaelder@uwaterloo.ca>
2017-11-21 08:37:00 -0500

    Implemented querystring in pc redirect router to pass query to pc2

--- src/server/routes/pc.js
+++ src/server/routes/pc.js
@@ -1,9 +1,10 @@
 //Import Depedencies
 const express = require('express');
+const qs = require('querystring');
 const router = express.Router();
 
 router.get('/:path', function(req,res){
-  res.redirect('http://www.pathwaycommons.org/pc2/' + req.params.path);
+  res.redirect('http://www.pathwaycommons.org/pc2/' + req.params.path+ '?' + qs.stringify(req.query));
 });
 
 module.exports = router;

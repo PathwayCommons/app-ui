75618e51c7f633c2369445b603fce5f617fc88ef
Dylan Fong <d2fong@uwaterloo.ca>
2017-11-22 12:16:34 -0500

    Only remove parent complex node labels

--- src/client/common/cy/stylesheet.js
+++ src/client/common/cy/stylesheet.js
@@ -22,7 +22,7 @@ const stylesheet = sbgnStyleSheet(cytoscape)
 .css({
   'width': 45,
   'height': 45,
-  'label': node => ''
+  'label': node => node.isParent() ? '' : node.data('label')
 })
 .selector('.compoundcollapse-collapsed-node')
 .css({

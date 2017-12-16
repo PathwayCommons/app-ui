10aba609f32cc4cd46ec0ed831d7fd73a965dc3d
d2fong <d2fong@uwaterloo.ca>
2017-12-15 19:51:43 -0500

    Refactor paint drawer to be a menu component that is injected into the base component view

--- src/client/features/paint/index.js
+++ src/client/features/paint/index.js
@@ -11,7 +11,8 @@ const make_cytoscape = require('../../common/cy');
 const { ServerAPI } = require('../../services');
 
 
-const {createExpressionTable} = require('./expression-model');
+const {createExpressionTable, applyExpressionData} = require('./expression-model');
+const PaintMenu = require('./paint-menu');
 
 const PaintViewConfig = {
   toolbarButtons: BaseNetworkView.config.toolbarButtons.concat({
@@ -21,7 +22,11 @@ const PaintViewConfig = {
     menuId: 'paintMenu',
     description: 'View expression data'
   }),
-  menus: BaseNetworkView.config.menus
+  menus: BaseNetworkView.config.menus.concat({
+    id: 'paintMenu',
+    func: props => h(PaintMenu, props)
+
+  })
 };
 
 
@@ -40,16 +45,17 @@ class Paint extends React.Component {
         comments: []
       },
 
-      enrichmentsLoading: true,
+      expressionsLoading: true,
       networkLoading: true,
-
-
 
       // enrichment data state
       rawEnrichmentData: {},
       expressionTable: {},
       selectedClass: '',
-      // selectedFunction: this.analysisFns().mean,
+      selectedFunction: {
+        name: 'mean',
+        func: _.mean
+      },
 
       // search results state
       query: '',
@@ -89,95 +95,44 @@ class Paint extends React.Component {
     .then(json => {
       const expressionClasses = _.get(json.dataSetClassList, '0.classes', []);
       const expressions = _.get(json.dataSetExpressionList, '0.expressions', []);
-      
-      this.state.cy.on('network-loaded', () => this.initPainter(expressions, expressionClasses));
+
+      this.initPainter(expressions, expressionClasses);
     });
-  }
-
-  expressionDataToNodeStyle(value, range) {
-    const [, max] = range;
-    const style = {};
-
-    if ((0 - max / 3) <= value < (0 + max / 3)) {
-      style['background-color'] = 'white';
-      style['background-opacity'] = 1;
-      style['color'] = 'black';
-    }
-
-    if (value < (0 - max / 3)) {
-      style['background-opacity'] = `${Math.abs(value / max)}`;
-      style['background-color'] = 'green';
-      style['color'] = 'white';
-      style['text-outline-color'] = 'black';
-    }
-
-    if ((0 + max / 3) <= value ) {
-      style['background-color'] = 'purple';
-      style['background-opacity'] = `${value / max}`;
-      style['color'] = 'white';
-      style['text-outline-color'] = 'black';
-
-    }
-    return style;
-  }
-
-  computeFoldChange(expression, selectedFunction) {
-    const classValues = Object.entries(expression.classValues);
-    const c1Val = selectedFunction(classValues[0][1]);
-
-    let c2Val = selectedFunction(classValues[1][1]);
-    c2Val = c2Val === 0 ? c2Val = 1 : c2Val;
-
-    let foldChange = Math.log2(c1Val / c2Val);
-
-    return {
-      geneName: expression.geneName,
-      value: parseFloat(foldChange.toFixed(2))
-    };
   }
 
   initPainter(expressions, expressionClasses) {
     const state = this.state;
     const expressionTable = createExpressionTable(expressions, expressionClasses);
-    const selectedFn = _.mean;
-
-    const geneNodes = state.cy.nodes('[class="macromolecule"]');
-    const geneNodeLabels = _.uniq(geneNodes.map(node => node.data('label'))).sort();
-
-    const expressionsInNetwork = expressionTable.rows.filter(row => geneNodeLabels.includes(row.geneName));
-
-    const expressionLabels = expressionsInNetwork.map(expression => expression.geneName);
-    geneNodes.filter(node => !expressionLabels.includes(node.data('label'))).style({
-      'background-color': 'grey',
-      'color': 'grey',
-      'opacity': 0.4
+    this.setState({
+      expressionTable: expressionTable,
+      expressionsLoading: false      
     });
 
-    const foldValues = expressionsInNetwork.map(expression => this.computeFoldChange(expression, selectedFn));
-    const fvs = foldValues.map(fv => fv.value);
-    const maxMagnitude = Math.max(Math.max(...fvs), Math.abs(Math.min(...fvs)));
-    const range = [-maxMagnitude, maxMagnitude];
-    foldValues.forEach(fv => {
-      const matchedNodes = state.cy.nodes().filter(node => node.data('label') === fv.geneName);
-      const style = this.expressionDataToNodeStyle(fv.value, range);
 
-      state.cy.batch(() => matchedNodes.style(style));
-    });
+    const selectedFn = state.selectedFunction.func;
+
+    this.state.cy.on('network-loaded', () => applyExpressionData(this.state.cy, expressionTable, selectedFn));
   }
 
   render() {
     const state = this.state;
 
     const baseView = h(BaseNetworkView.component, {
+      // generic base view props
       layoutConfig: state.layoutConfig,
       componentConfig: state.componentConfig,
       cy: state.cy,
       networkJSON: state.networkJSON,
-      networkMetadata: state.networkMetadata
+      networkMetadata: state.networkMetadata,
+
+      // paint specific props needed by the paint menu
+      expressionTable: state.expressionTable,
+      selectedFunction: state.selectedFunction
+
     });
 
     // create a view shell loading view e.g looks like the view but its not
-    const content = state.enrichmentsLoading && state.networkLoading ? h('div', 'Loading') : baseView;
+    const content = state.expressionsLoading || state.networkLoading ? h('div', 'Loading') : baseView;
 
     return h('div', [content]);
   }

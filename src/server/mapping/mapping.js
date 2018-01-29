eab2ffc4fd36bb0b178d4c59235796bfe22e2966
Wanxin Li <wanxinli@MacBookAir5.local>
2018-01-29 14:45:36 -0500

    added token class
    added validate.js, validate encapsulates 3 operations, proceed checks if the input is ready for the next service,
    passed all test cases

--- src/server/mapping/mapping.js
+++ src/server/mapping/mapping.js
@@ -1,18 +1,18 @@
 
 const fs = require("fs");
 const path = require("path");
-const jsonData = fs.readFileSync(path.resolve(__dirname, "Validation_test_data_set.json"));
+const jsonData = fs.readFileSync(path.resolve(__dirname, "HGNC.json"));
 const jsonContent = JSON.parse(jsonData);
 
 
 // map HGNC symbol to HGNC ID
 // After validation step, we can assume all symbols are valid
 // so that we can find a mapping for sure
-const mapping = function(symbol) {
+const mapping = function(hgncSymbol) {
   let id = 0;
   jsonContent.forEach(element => {
-    if (element['FIELD1'] === symbol) {
-      id = element['FIELD2'];
+    if (element['Approved_Symbol'] === hgncSymbol) {
+      id = element['HGNC_ID'];
     }
   });
   return id;

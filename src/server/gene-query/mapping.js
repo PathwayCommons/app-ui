c4adc6722234dd34530c869796add3f62d519ba8
Wanxin Li <wanxinli@MacBookAir5.local>
2018-02-01 17:02:18 -0500

    changed jsonContent to a map

--- src/server/gene-query/mapping.js
+++ src/server/gene-query/mapping.js
@@ -1,22 +1,25 @@
 
 const fs = require("fs");
 const path = require("path");
-const jsonData = fs.readFileSync(path.resolve(__dirname, "HGNC.json"));
-const jsonContent = JSON.parse(jsonData);
+const jsonData = JSON.parse(fs.readFileSync(path.resolve(__dirname, "HGNC_dataset.json")));
+const jsonContent = new Map();
+jsonData.forEach(el => jsonContent.set(el['B'], el['A']));
 
 
 // map HGNC symbol to HGNC ID
 // After validation step, we can assume all symbols are valid
 // so that we can find a mapping for sure
-const mapping = function(hgncSymbol) {
-  let id = null;
-  jsonContent.forEach(element => {
-    if (element['Approved_Symbol'] === hgncSymbol) {
-      id = element['HGNC_ID'];
-    }
-  });
-  return id;
+const mapping = function (hgncSymbol) {
+  const validSymbol = jsonContent.has(hgncSymbol);
+  if(validSymbol) {
+    return jsonContent.get(hgncSymbol);
+  }
+  return null;
 };
+
+
+
+
 
 
 module.exports = mapping;

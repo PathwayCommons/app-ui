7aebcc218a758cb2464d4682c98e4d2c71b6318e
Wanxin Li <wanxinli@MacBookAir5.local>
2018-02-27 14:53:30 -0500

    removed promise-simple

--- src/server/enrichment/validate.js
+++ src/server/enrichment/validate.js
@@ -1,38 +1,21 @@
 const request = require('request');
-const Promise = require('promise-simple');
 const _ = require('lodash');
-const validateGp = function (query, userSetting) {
-  const defaultSetting = {
-    "output": "mini",
-    "organism": "hsapiens",
-    "significant": "1",
-    "sort_by_structure": "1",
-    "ordered_query": "0",
-    "as_ranges": "0",
-    "no_iea": "1",
-    "underrep": "0",
-    "hierfiltering": "none",
-    "user_thr": "1",
-    "min_set_size": "5",
-    "max_set_size": "200",
-    "threshold_algo": "fdr",
-    "domain_size_type": "annotated",
-    "custbg_cb": "none",
-    "sf_GO:BP": "1",
-    "sf_REAC": "1",
-    "query": query
-  };
+const validateGp = function (query) {
+  const promise = new Promise(function (resolve, reject) {
+    const defaultSetting = {
+      "output": "mini",
+      "query": query
+    };
 
-  const formData = _.assign({}, defaultSetting, userSetting);
-  const d = Promise.defer();
-  request.post({ url: "http://biit.cs.ut.ee/gprofiler/gconvert.cgi", formData: formData }, function optionalCallback(err, httpResponse, body) {
-    if (err) {
-      d.reject(err);
-    }
-
-    d.resolve(body);
+    const formData = _.assign({}, defaultSetting);
+    request.post({ url: "http://biit.cs.ut.ee/gprofiler/gconvert.cgi", formData: formData }, function optionalCallback(err, httpResponse, body) {
+      if (err) {
+        reject(err);
+      }
+      resolve(body);
+    });
   });
-  return d;
+  return promise;
 };
 
 

bf59096cc5b9302c8e23be018162aa4441caec18
logmosier <logmosier@gmail.com>
2018-03-06 16:08:12 -0500

    Added a title to the filter menu

--- src/client/common/components/base-network-view/config/menus/interactions-filter.js
+++ src/client/common/components/base-network-view/config/menus/interactions-filter.js
@@ -7,10 +7,12 @@ class InteractionsFilterMenu extends React.Component {
    const buttons= [...props.buttons].map(([type, clicked])=>
    h('div',{key:type,className:classNames ('interaction-filter-button',clicked? 'interaction-filter-clicked':'interaction-filter-not-clicked'),onClick: () => props.filterUpdate(type)},[
         h('div',{className:classNames(type,'interaction-filter-legend')}),
-        h('h2.button-label',type),
+        h('h3.button-label',type),
         h('i', {className: classNames('common-icon-button','material-icons',{ 'common-icon-button-active': !clicked})},clicked ? 'close':'check')
       ]));
-    return h('div',buttons);
+    return h('div',[
+      h('h2', 'Interaction Filters'),
+      buttons]);
     }
   
 }

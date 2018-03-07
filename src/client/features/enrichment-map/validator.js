96720aaed164e5d58c84a1ed85de673076c977db
wxli0 <wanxinli@MacBookAir5.local>
2018-03-07 15:12:52 -0500

    fill in UI skeleton relates to #468

--- src/client/features/enrichment-map/validator.js
+++ src/client/features/enrichment-map/validator.js
@@ -1,9 +1,34 @@
 const React = require('react');
 const h = require('react-hyperscript');
+const { ServerAPI } = require('../../services');
 
 class ValidatorServiceUI extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: 'no result yet',
+      query: ''
+    };
+  }
+  validatorGconvert() {
+    ServerAPI.validatorGconvert(this.state.query).then(res => {
+      this.setState({ result_validate_gp: 'validator result from gConvert is ' + JSON.stringify(res) });
+    });
+  }
   render() {
-    return h('div', 'VALIDATOR SERVICE UI');
+    return h('div',
+      [
+        h('div', [h('input', {
+          placeholder: 'enter genes: a list of gene names separated by whitespace', onChange: e => this.setState({ query: e.target.value }),
+          style: {
+            width: '100vw',
+          }
+        })]),
+        h('div', [h('button', { onClick: e => this.validatorGconvert() }, 'click to validate')]),
+        this.state.result
+      ]);
   }
 }
+
+
 module.exports = ValidatorServiceUI;

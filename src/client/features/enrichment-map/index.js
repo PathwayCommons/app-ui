64c2d2b9cbbdaf15fc2cd10603179eacb5a83992
Dylan Fong <d2fong@uwaterloo.ca>
2018-03-07 13:37:46 -0500

    Hook up react tabs so that you can switch between different uis for the different enrichment map services

--- src/client/features/enrichment-map/index.js
+++ src/client/features/enrichment-map/index.js
@@ -1,9 +1,27 @@
 const React = require('react');
 const h = require('react-hyperscript');
+const {Tab, Tabs, TabList, TabPanel} = require('react-tabs');
 
 class EnrichmentMap extends React.Component {
   render() {
-    return h('div', 'ENRICHMENT MAP');
+    return h('div', [
+      h(Tabs, [
+        h(TabList, [
+          h(Tab, 'Validator'),
+          h(Tab, 'Enrichment'),
+          h(Tab, 'EMap')
+        ]),
+        h(TabPanel, [
+          h('div', 'Validator skeleton')
+        ]),
+        h(TabPanel, [
+          h('div', 'Enrichment skeleton')
+        ]),
+        h(TabPanel, [
+          h('div', 'EMap skeleton')
+        ]),
+      ])
+    ]);
   }
 }
 module.exports = EnrichmentMap;

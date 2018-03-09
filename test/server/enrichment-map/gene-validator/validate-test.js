b1ec8f03864caf6876c0045f5afe1d92eee73b70
wxli0 <wanxinli@MacBookAir5.local>
2018-03-09 09:50:01 -0500

    modify test for gene-validator

--- test/server/enrichment-map/gene-validator/validate-test.js
+++ test/server/enrichment-map/gene-validator/validate-test.js
@@ -4,8 +4,8 @@ const {validatorGconvert} = require('../../../../src/server/enrichment-map/gene-
 
 describe('test validatorGconvert', function() {
   it('it should return an object', function() {
-    return (validatorGconvert('TP53')).then(function(res) {
-      expect(res).to.deep.equal({"unrecogized":[],"duplicate":[],"geneInfo":[{"HGNC_symbol":"TP53","HGNC_id":"HGNC:11998"}]});
+    return (validatorGconvert('TP53 ATP ATM TP53')).then(function(res) {
+      expect(res).to.deep.equal({"unrecogized":["ATP"],"duplicate":["TP53"],"geneInfo":[{"HGNC_symbol":"TP53","HGNC_id":"HGNC:11998"},{"HGNC_symbol":"ATM","HGNC_id":"HGNC:795"},{"HGNC_symbol":"TP53","HGNC_id":"HGNC:11998"}]});
     });
   });
 });

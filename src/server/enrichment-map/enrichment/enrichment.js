d5f31f93fc7d120e2971977a3b34f32a47b1ae73
wxli0 <wanxinli@MacBookAir5.local>
2018-03-21 11:05:52 -0400

    report duplicate, unrecognized from gprofiler wrapper call relates to #537

--- src/server/enrichment-map/enrichment/enrichment.js
+++ src/server/enrichment-map/enrichment/enrichment.js
@@ -13,8 +13,6 @@ const _ = require('lodash');
 // remove #WARNING, #INFO, and the first line
 const parseGProfilerResponse = (gProfilerResponse) => {
   // remove the second line
-  const lines = gProfilerResponse.split('\n');
-  lines.slice(0, 1);
   const str1 = gProfilerResponse.split('\n').slice(0, 1).join("\n");
   let str2 = gProfilerResponse.split('\n').slice(2).join("\n"); // concatenate at last
   // remove lines starting with #
@@ -23,6 +21,12 @@ const parseGProfilerResponse = (gProfilerResponse) => {
   return str4;
 };
 
+// extract #WARNING from response
+const extractWarning = (gProfilerResponse) => {
+  const warningLines = gProfilerResponse.replace(/^(?!#WARNING).*$/mg, "");
+  const warningInfo = _.filter(warningLines.split('\n'), ele => ele.length != 0);
+  return warningInfo;
+};
 
 const defaultSetting = {
   "output": "mini",
@@ -55,6 +59,19 @@ const enrichment = (query, userSetting) => {
         reject(err);
       }
 
+      const unrecognized = [];
+      const duplicate = [];
+      const warningInfo = extractWarning(gProfilerResponse);
+      _.forEach(warningInfo, ele => {
+        const desIndex = 1;
+        const hgncSymbolIndex = 1;
+        if (ele.indexOf('same internal ID') > -1) {
+          duplicate.push(ele.split('\t')[desIndex].split(/\s+/)[hgncSymbolIndex]);
+        } else if (ele.indexOf('not recognized') > -1) {
+          unrecognized.push(ele.split('\t')[desIndex].split(/\s+/)[hgncSymbolIndex]);
+        }
+      });
+
       let responseInfo = parseGProfilerResponse(gProfilerResponse).split('\n');
       responseInfo.splice(0, 1); // remove first elem
       responseInfo = _.map(responseInfo, ele => ele.split('\t'));
@@ -74,12 +91,23 @@ const enrichment = (query, userSetting) => {
       const tNameIndex = 11;
       const tDepthIndex = 12;
       const QTListIndex = 13;
+      ret.duplicate = duplicate;
+      ret.unrecognized = unrecognized;
+      ret.orderedQuery = formData.ordered_query;
+      ret.userThr = formData.user_thr;
+      ret.minSetSize = formData.min_set_size;
+      ret.maxSetSize = formData.max_set_size;
+      ret.thresholdAlgo = formData.threshold_algo;
+      ret.custbg = formData.custbg;
+      ret.custbgCb = formData.custbg_cb;
+      ret.pathwayInfo = {};
       _.forEach(responseInfo, elem => {
-        ret[elem[termIdIndex]] = { signf: elem[signfIndex], pvalue: elem[pvalueIndex], T: elem[TIndex], Q: elem[QIndex], tType: elem[tTypeIndex], tGroup: elem[tGroupIndex], tName: elem[tNameIndex], tDepth: elem[tDepthIndex] };
-        ret[elem[termIdIndex]]["Q&T"] = elem[QTIndex];
-        ret[elem[termIdIndex]]["Q&T/Q"] = elem[QTQIndex];
-        ret[elem[termIdIndex]]["Q&T/T"] = elem[QTTIndex];
-        ret[elem[termIdIndex]]["Q&TList"] = elem[QTListIndex];
+        let termIdInfo = { signf: elem[signfIndex], pvalue: elem[pvalueIndex], T: elem[TIndex], Q: elem[QIndex], tType: elem[tTypeIndex], tGroup: elem[tGroupIndex], tName: elem[tNameIndex], tDepth: elem[tDepthIndex] };
+        termIdInfo["Q&T"] = elem[QTIndex];
+        termIdInfo["Q&T/Q"] = elem[QTQIndex];
+        termIdInfo["Q&T/T"] = elem[QTTIndex];
+        termIdInfo["Q&TList"] = elem[QTListIndex];
+        ret.pathwayInfo[elem[termIdIndex]] = termIdInfo;
       });
       resolve(ret);
     });
@@ -90,6 +118,3 @@ const enrichment = (query, userSetting) => {
 
 module.exports = { enrichment };
 
-// enrichment(['AFF4']).then(function (results) {
-//    console.log(results);
-// });

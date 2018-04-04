5592531a25c5769c53072da5f80cd745db620247
wxli0 <wanxinli@MacBookAir5.local>
2018-04-04 11:36:20 -0400

    updated tests for distiller related to #597

--- test/server/enrichment-map/enrichment/enrichment-test.js
+++ test/server/enrichment-map/enrichment/enrichment-test.js
@@ -19,71 +19,93 @@ describe('test enrichment', function () {
   this.timeout(500000);
   it('it should return an object', function () {
     return (enrichment('AFF4')).then(function (res) {
-      const result = { 'GO:0006354':
-      { signf: '!',
-        pvalue: '8.70e-03',
-        T: '124',
-        Q: '1',
-        tType: 'BP',
-        tGroup: '5',
-        tName: '   DNA-templated transcription, elongation',
-        tDepth: '1',
-        'Q&T': '1',
-        'Q&T/Q': '1.000',
-        'Q&T/T': '0.008',
-        'Q&TList': 'AFF4' },
-     'GO:0006368':
-      { signf: '!',
-        pvalue: '8.70e-03',
-        T: '100',
-        Q: '1',
-        tType: 'BP',
-        tGroup: '5',
-        tName: '    transcription elongation from RNA polymerase II promoter',
-        tDepth: '2',
-        'Q&T': '1',
-        'Q&T/Q': '1.000',
-        'Q&T/T': '0.010',
-        'Q&TList': 'AFF4' },
-     'REAC:75955':
-      { signf: '!',
-        pvalue: '7.75e-03',
-        T: '61',
-        Q: '1',
-        tType: 'rea',
-        tGroup: '2',
-        tName: '   RNA Polymerase II Transcription Elongation',
-        tDepth: '1',
-        'Q&T': '1',
-        'Q&T/Q': '1.000',
-        'Q&T/T': '0.016',
-        'Q&TList': 'AFF4' },
-     'REAC:112382':
-      { signf: '!',
-        pvalue: '7.75e-03',
-        T: '61',
-        Q: '1',
-        tType: 'rea',
-        tGroup: '2',
-        tName: '    Formation of RNA Pol II elongation complex ',
-        tDepth: '2',
-        'Q&T': '1',
-        'Q&T/Q': '1.000',
-        'Q&T/T': '0.016',
-        'Q&TList': 'AFF4' },
-     'REAC:674695':
-      { signf: '!',
-        pvalue: '7.75e-03',
-        T: '83',
-        Q: '1',
-        tType: 'rea',
-        tGroup: '6',
-        tName: '   RNA Polymerase II Pre-transcription Events',
-        tDepth: '1',
-        'Q&T': '1',
-        'Q&T/Q': '1.000',
-        'Q&T/T': '0.012',
-        'Q&TList': 'AFF4' } };
+      const result = {
+        "options": {
+          "orderedQuery": 0,
+          "userThr": 0.05,
+          "minSetSize": 5,
+          "maxSetSize": 200,
+          "thresholdAlgo": "fdr",
+          "custbg": []
+        },
+        "pathwayInfo": {
+          "GO:0006354": {
+            "pValue": 0.0087,
+            "t": 124,
+            "q": 1,
+            "qAndT": 1,
+            "qAndTOverQ": 1,
+            "qAndTOverT": 0.008,
+            "tType": "BP",
+            "tGroup": 1,
+            "tName": "DNA-templated transcription, elongation",
+            "tDepth": 1,
+            "qAndTList": [
+              "AFF4"
+            ]
+          },
+          "GO:0006368": {
+            "pValue": 0.0087,
+            "t": 100,
+            "q": 1,
+            "qAndT": 1,
+            "qAndTOverQ": 1,
+            "qAndTOverT": 0.01,
+            "tType": "BP",
+            "tGroup": 1,
+            "tName": "transcription elongation from RNA polymerase II promoter",
+            "tDepth": 2,
+            "qAndTList": [
+              "AFF4"
+            ]
+          },
+          "REAC:674695": {
+            "pValue": 0.00775,
+            "t": 83,
+            "q": 1,
+            "qAndT": 1,
+            "qAndTOverQ": 1,
+            "qAndTOverT": 0.012,
+            "tType": "rea",
+            "tGroup": 3,
+            "tName": "RNA Polymerase II Pre-transcription Events",
+            "tDepth": 1,
+            "qAndTList": [
+              "AFF4"
+            ]
+          },
+          "REAC:75955": {
+            "pValue": 0.00775,
+            "t": 61,
+            "q": 1,
+            "qAndT": 1,
+            "qAndTOverQ": 1,
+            "qAndTOverT": 0.016,
+            "tType": "rea",
+            "tGroup": 2,
+            "tName": "RNA Polymerase II Transcription Elongation",
+            "tDepth": 1,
+            "qAndTList": [
+              "AFF4"
+            ]
+          },
+          "REAC:112382": {
+            "pValue": 0.00775,
+            "t": 61,
+            "q": 1,
+            "qAndT": 1,
+            "qAndTOverQ": 1,
+            "qAndTOverT": 0.016,
+            "tType": "rea",
+            "tGroup": 2,
+            "tName": "Formation of RNA Pol II elongation complex",
+            "tDepth": 2,
+            "qAndTList": [
+              "AFF4"
+            ]
+          }
+        }
+      };
       expect(gProfilerResEquality(result, res)).to.equal(true);
     });
   });
--- test/server/enrichment-map/gene-validator/validate-test.js
+++ test/server/enrichment-map/gene-validator/validate-test.js
@@ -5,7 +5,29 @@ const {validatorGconvert} = require('../../../../src/server/enrichment-map/gene-
 describe('test validatorGconvert', function() {
   it('it should return an object', function() {
     return (validatorGconvert('TP53 ATP ATM TP53')).then(function(res) {
-      expect(res).to.deep.equal({"unrecogized":["ATP"],"duplicate":["TP53"],"geneInfo":[{"HGNC_symbol":"TP53","HGNC_id":"HGNC:11998"},{"HGNC_symbol":"ATM","HGNC_id":"HGNC:795"},{"HGNC_symbol":"TP53","HGNC_id":"HGNC:11998"}]});
+      const result = {
+        "options": {
+          "target": "HGNC",
+          "organism": "hsapiens"
+        },
+        "unrecognized": [
+          "ATP"
+        ],
+        "duplicate": [
+          "TP53"
+        ],
+        "geneInfo": [
+          {
+            "initialAlias": "TP53",
+            "convertedAlias": "HGNC:11998"
+          },
+          {
+            "initialAlias": "ATM",
+            "convertedAlias": "HGNC:795"
+          }
+        ]
+      };
+      expect(res).to.deep.equal(result);
     });
   });
 });

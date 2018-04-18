1dbf02c4c777452c7cb86b23f035a8fa329c1d00
logmosier <logmosier@gmail.com>
2018-04-18 12:32:43 -0400

    WIP style changes

--- src/client/features/interactions/filter-menu.js
+++ src/client/features/interactions/filter-menu.js
@@ -13,9 +13,9 @@ class InteractionsFilterMenu extends React.Component {
         onClick: () => props.filterUpdate(type)
       },
       [
-        h('div',{className:classNames(type,'interaction-filter-legend')}),
+        h('div',{className:classNames('interaction-filter-legend',{[type]:!clicked})}),
         h('h3.button-label',type),
-        h('i', {className: classNames('common-icon-button','material-icons','icon-cutoff',{ 'common-icon-button-active': !clicked})}, clicked ? 'close':'check')
+        //h('i', {className: classNames('common-icon-button','material-icons','icon-cutoff',{ 'common-icon-button-active': !clicked})}, clicked ? 'close':'check')
       ]
     ));
     return h('div',[

afa15ae2a8bde8adcd4d70c3b75dbe0a042b5f54
wxli0 <wanxinli@MacBookAir5.local>
2018-04-26 13:27:04 -0400

    remove comments

--- src/server/enrichment/helper/map-react-id.js
+++ src/server/enrichment/helper/map-react-id.js
@@ -1,6 +1,5 @@
 const mapReactId = (identifier) => {
   if (identifier.substring(0, 5) === "REAC:") {
-    console.log(identifier);
     return "R-HSA-" + identifier.substring(5, identifier.length);
   }
   return identifier;

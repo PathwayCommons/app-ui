f5df87446faa3820e3596cf12c9102733326a484
Caitlin <caitlinocallaghan22@gmail.com>
2018-05-29 13:21:16 -0400

    enrichment test case update

--- test/server/enrichment/analysis/analysis-test.js
+++ test/server/enrichment/analysis/analysis-test.js
@@ -6,8 +6,10 @@ const { enrichment } = require('../../../../src/server/enrichment/analysis');
 
 describe('test enrichment', function () {
   this.timeout(500000);
-  it('it should return an object', function () {
-    return (enrichment(['AFF4'])).then(function (res) {
+  it('only input gene', function () {
+    return (enrichment(['AFF4'])).then(
+      //resolved
+      function (res) {
       const result = {
         "pathwayInfo": {
           "GO:0006354": {
@@ -48,6 +50,77 @@ describe('test enrichment', function () {
         }
       };
       expect(res).to.deep.equal(result);
-    });
+    },
+    //rejected
+    rej => {
+    console.log("rejected");
+    }
+    );
   });
+
+  it('all valid parameters', function () {
+    return (enrichment(['AFF4'], {orderedQuery: false, userThr: 0.07, minSetSize: 3, maxSetSize:400, thresholdAlgo: "fdr", custbg: []})).then(
+      //resolved
+      res=> {
+      const result = {
+        "pathwayInfo": {
+          "GO:0006354": {
+            "p-value": 0.0087,
+            "description": "DNA-templated transcription, elongation",
+            "intersection": [
+              "AFF4"
+            ]
+          },
+          "GO:0006368": {
+            "p-value": 0.0087,
+            "description": "transcription elongation from RNA polymerase II promoter",
+            "intersection": [
+              "AFF4"
+            ]
+          },
+          "REAC:674695": {
+            "p-value": 0.00775,
+            "description": "RNA Polymerase II Pre-transcription Events",
+            "intersection": [
+              "AFF4"
+            ]
+          },
+          "REAC:75955": {
+            "p-value": 0.00775,
+            "description": "RNA Polymerase II Transcription Elongation",
+            "intersection": [
+              "AFF4"
+            ]
+          },
+          "REAC:112382": {
+            "p-value": 0.00775,
+            "description": "Formation of RNA Pol II elongation complex",
+            "intersection": [
+              "AFF4"
+            ]
+          }
+        }
+      };
+      expect(res).to.deep.equal(result);
+    },
+    //rejected
+    rej => {
+    console.log("rejected")
+    }
+    );
+  });
+
+  it('INVALID parameters', function () {
+    return (enrichment(['AFF4'], {orderedQuery: false, minSetSize: 3, maxSetSize:400, thresholdAlgo: "fddddr", custbg: []})).then(
+      //resolved
+      res => {
+      console.log("resolved");
+      },
+      //rejected
+      rej => {
+      expect(true);
+      }
+    );
+  });
+
 });

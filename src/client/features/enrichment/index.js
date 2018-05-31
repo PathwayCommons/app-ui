bbc67e06e2a5ea8c2d4ec1fe2847ce06211441f0
Caitlin <caitlinocallaghan22@gmail.com>
2018-05-31 15:20:38 -0400

    further edits to index.js skeleton

--- src/client/features/enrichment/index.js
+++ src/client/features/enrichment/index.js
@@ -4,10 +4,10 @@ const queryString = require('query-string');
 const _ = require('lodash');
 const Loader = require('react-loader');
 
-class EnrichmentMap extends React.Component {
+class Enrichment extends React.Component {
   render() {
     //test title to verify that page has loaded, will appear in top right corner
     return h('div', "Enrichment");
   }
 }
-module.exports = EnrichmentMap;
+module.exports = Enrichment;

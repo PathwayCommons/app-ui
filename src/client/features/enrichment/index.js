e2ac918a39d15f193288748a01ba1c1a4755c5ba
Caitlin <caitlinocallaghan22@gmail.com>
2018-06-04 16:22:36 -0400

    menubar skeleton (gene input and node search with proper icons)

--- src/client/features/enrichment/index.js
+++ src/client/features/enrichment/index.js
@@ -1,13 +1,54 @@
 const React = require('react');
 const h = require('react-hyperscript');
-const queryString = require('query-string');
+//const queryString = require('query-string');
 const _ = require('lodash');
-const Loader = require('react-loader');
+//const Loader = require('react-loader');
+
+// const hideTooltips = require('../../common/cy/events/click').hideTooltips;
+// const removeStyle= require('../../common/cy/manage-style').removeStyle;
+// const make_cytoscape = require('../../common/cy/');
+// const interactionsStylesheet= require('../../common/cy/interactions-stylesheet');
+//const { ServerAPI } = require('../../services/');
+const { BaseNetworkView } = require('../../common/components');
+//const { getLayoutConfig } = require('../../common/cy/layout');
+//const downloadTypes = require('../../common/config').downloadTypes;
+
+const enrichmentConfig={
+  //extablish toolbar and declare features to not include
+  toolbarButtons: _.differenceBy(BaseNetworkView.config.toolbarButtons,[{'id': 'expandCollapse'}, {'id': 'showInfo'}],'id'),
+  menus: BaseNetworkView.config.menus,
+  //allow for searching of nodes
+  useSearchBar: true,
+  //display gene input rather than pathway or database name
+  useGeneInput: true
+};
 
 class Enrichment extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      componentConfig: enrichmentConfig,
+      networkMetadata: {
+        name: '',
+        datasource: '',
+        comments: []
+      }
+    };
+
+    }
+
   render() {
-    //test title to verify that page has loaded, will appear in top right corner
-    return h('div', "Enrichment");
+    const state = this.state;
+    const baseView = h(BaseNetworkView.component, {
+      componentConfig: state.componentConfig,
+      //titles at top of toolbar
+      networkMetadata: {}
+    });
+
+    return h('div.main', [baseView]);
   }
 }
 module.exports = Enrichment;
+
+//NOTE: CURRENTLY ONLY RENDERS ON PAGE WHEN base-network-view.js function 'componentDidMount(){}'
+//      IS COMMENTED OUT

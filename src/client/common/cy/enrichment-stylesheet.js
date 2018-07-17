381f28a39a9837fae481a87d9c8a5fe34e7234d2
Caitlin <caitlinocallaghan22@gmail.com>
2018-07-17 09:51:04 -0400

    use const and function name change

--- src/client/common/cy/enrichment-stylesheet.js
+++ src/client/common/cy/enrichment-stylesheet.js
@@ -1,30 +1,30 @@
 const cytoscape = require('cytoscape');
 
-var p_valueColorScale = [
+const p_valueColorScale = [
   { p_value: 0, color: { r: 0, g: 0, b: 255 } },
   { p_value: .05, color: { r: 179, g: 230, b: 255 } }
  ];
 
-var getColorForP_Value = function(p_value) {
+function getNodeColor( p_value ){
 
   if( p_value > 0.05 ) return '#555';
 
   //NOTE: if > 2 colors in scale, iterate through p_valueColorScale.p_value to find upper and lower color bounds
-  var lowerColor = p_valueColorScale[0];
-  var upperColor = p_valueColorScale[1];
-  var colorRange = upperColor.p_value - lowerColor.p_value;
+  const lowerColor = p_valueColorScale[0];
+  const upperColor = p_valueColorScale[1];
+  const colorRange = upperColor.p_value - lowerColor.p_value;
 
   //create a linear scale with upper and lower colors
-  var rangePct = (p_value - lowerColor.p_value) / colorRange;
-  var pctLower = 1 - rangePct;
-  var pctUpper = rangePct;
-  var color = {
+  const rangePct = (p_value - lowerColor.p_value) / colorRange;
+  const pctLower = 1 - rangePct;
+  const pctUpper = rangePct;
+  const color = {
       r: Math.floor(lowerColor.color.r * pctLower + upperColor.color.r * pctUpper),
       g: Math.floor(lowerColor.color.g * pctLower + upperColor.color.g * pctUpper),
       b: Math.floor(lowerColor.color.b * pctLower + upperColor.color.b * pctUpper)
   };
   return 'rgb(' + [color.r, color.g, color.b].join(',') + ')';
-};
+}
 
 function getNodeSize( geneCount ){
   return mapGeneCountToSize(Math.min( geneCount, 1000 ));
@@ -53,7 +53,7 @@ const enrichmentStylesheet=cytoscape.stylesheet()
 .css({
   'font-size': 20,
   'color': 'black',
-  'background-color': node => getColorForP_Value(node.data('p_value')),
+  'background-color': node => getNodeColor(node.data('p_value')),
   'background-opacity':0.8,
   'text-outline-color': 'white',
   'text-outline-width': 2,

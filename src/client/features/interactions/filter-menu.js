8b67ac510c11deed90b0c5845c5e23b69d115717
M Wrana <wranamichael@gmail.com>
2018-07-17 12:01:02 -0400

    Slider Kind of woking now

--- src/client/features/interactions/filter-menu.js
+++ src/client/features/interactions/filter-menu.js
@@ -6,7 +6,14 @@ const _ = require('lodash');
 class InteractionsFilterMenu extends React.Component {
   constructor(props) {
     super(props);
+    this.state = _.merge({defaultSliderVal:0,maxSliderVal:0},props);
     this.sliderUpdate = _.debounce(this.sliderUpdate,150);
+
+    this.state.cySrv.loadPromise().then( cy => {
+      let defaultResult = this.findDefaultAndUpdate(cy,20);
+      this.state.defaultSliderVal = defaultResult[0];
+      this.state.maxSliderVal = defaultResult[1];
+    });
   }
 
   /**
@@ -15,7 +22,9 @@ class InteractionsFilterMenu extends React.Component {
    * @description Hides nodes based on their betweenness centrality, determined by on-screen slider
    */
   sliderUpdate(){
-    const cy = this.props.cy;
+    const cy = this.props.cySrv.get();
+
+    console.log("HERE");
 
     //get the value from the slider
     let sliderVal = document.getElementById('selection-slider').value;
@@ -35,9 +44,8 @@ class InteractionsFilterMenu extends React.Component {
    * @description Hides all nodes based on betweenness centrality, keeping only `nodesToShow` visible.
    * Also sets the default value for the slider, based on this number.
    */
-  findDefaultAndUpdate(nodesToShow){
-    //setting up variables
-    const cy = this.props.cy;
+  findDefaultAndUpdate(cy,nodesToShow){
+
     let i = 0;
     let returnValue = 0;
     let maxVal = 0;
@@ -73,11 +81,9 @@ class InteractionsFilterMenu extends React.Component {
   render(){
     const props= this.props;
 
-
     //Networks end up with all nodes next to 0 bcVal other than search term
     //slider becomes a toggle since noone has the dexterity to distinguish values at the low end
     //instead use the second highest bcVal as the maximum for slider, so its actually useful
-    const [defaultSliderVal,maxSliderVal] = this.findDefaultAndUpdate(20);
     const buttons= _.map(props.filters,(active,type)=>
     h('div',{
         key:type,
@@ -92,9 +98,10 @@ class InteractionsFilterMenu extends React.Component {
 
     //Slider listed under 'Visible Nodes' in the interaction viewer
     const slider = [
-      h("input",{type:"range",id:'selection-slider',min:0,max:maxSliderVal,step:0.0001,defaultValue:defaultSliderVal,
+      h("input",{type:"range",id:'selection-slider',min:0,max:1,step:0.0001,defaultValue:0.9,
       onInput:() => this.sliderUpdate() }),
     ];
+    
 
     return h('div',[
       h('h2', 'Interaction Filters'),

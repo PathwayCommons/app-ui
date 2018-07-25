1cde2faf6e3db371d80bb33d7d17f397bf118048
Dylan Fong <d2fong@uwaterloo.ca>
2018-07-25 16:48:54 -0400

    remove default fetch opts from get publications call - it doesn't work with these opts for some reason

--- src/client/services/server-api/index.js
+++ src/client/services/server-api/index.js
@@ -23,7 +23,7 @@ const ServerAPI = {
   },
 
   getPubmedPublications( pubmedIds ){
-    return fetch('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&id=' + pubmedIds.toString(), defaultFetchOpts).then(res => res.json());
+    return fetch('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&id=' + pubmedIds.toString()).then(res => res.json());
   },
 
   getInteractionGraph(sources) {

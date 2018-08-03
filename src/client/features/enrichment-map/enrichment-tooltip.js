adc607b2e4f6a6b10c0733e6773e5fde311f4eab
Caitlin <caitlinocallaghan22@gmail.com>
2018-08-03 13:37:12 -0400

    store geneSet list in node data, database links

--- src/client/features/enrichment-map/enrichment-tooltip.js
+++ src/client/features/enrichment-map/enrichment-tooltip.js
@@ -7,9 +7,15 @@ class EnrichmentTooltip extends React.Component {
   render(){
     let {node, overviewDesc}= this.props;
     let title = node.data('description');
+    let id = node.data('id');
     let sharedGeneList = node.data('intersection');
-    let genesInPathway = node.data('intersection');
+    let sharedGeneCount = sharedGeneList.length;
     let geneCount = node.data('geneCount');
+    let geneSet = node.data('geneSet');
+    let dbInfo = {};
+
+    if( id.includes('GO') ) dbInfo = {name: 'Gene Ontology', url:'http://identifiers.org/go/' + id};
+    else dbInfo = {name: 'Reactome', url:'http://identifiers.org/reactome/' + id.replace("REAC:", "R-HSA-") };
 
     return h('div.enrichment-tooltip', [
       h('div.enrichment-tooltip-header',[
@@ -18,11 +24,13 @@ class EnrichmentTooltip extends React.Component {
       h('div.enrichment-tooltip-body', [
         h('div.enrichment-tooltip-field-name', 'Pathway Overview'),
         h('div.enrichment-tooltip-field-value', overviewDesc),
-        h('div.enrichment-tooltip-field-name', 'Genes Shared with Entered List (' + sharedGeneList.length + ')'),
-        h('div.enrichment-tooltip-field-value', sharedGeneList.join(' ')),
+        h('div.enrichment-tooltip-field-name', 'Genes Shared with Entered List (' + sharedGeneCount + ')'),
+        h('div.enrichment-tooltip-field-value', sharedGeneList.join(', ')),
         h('div.enrichment-tooltip-field-name', 'Genes in Pathway (' + geneCount + ')'),
-        h('div.enrichment-tooltip-field-value', genesInPathway.join(',')),
-        // h('a', { href: dbInfo.url }, dbInfo.name)
+        h('div.enrichment-tooltip-field-value', geneSet.join(', ')),
+      ]),
+      h('div.enrichment-tooltip-footer', [
+        h('a.plain-link', { href: dbInfo.url, target: '_blank', }, dbInfo.name)
       ])
     ]);
   }

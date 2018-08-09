9be6527f91b895be29f3154f36ecf804136ed905
Dylan Fong <dylanfong.ut@gmail.com>
2018-08-09 12:53:02 -0400

    fix client/config misnomer -- it just contained random data used by various components

---
+++ src/client/common/tippy-defaults.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: 'dark',
+  position: 'right',
+  animation: 'scale',
+  animateFill: false,
+  duration: [ 500, 0 ],
+  delay: [ 0, 0 ],
+  hideDuration: 0,
+  arrow: true,
+  trigger: 'mouseenter',
+  interactive: true,
+  multiple: true,
+  hideOnClick: true,
+  sticky: true
+};

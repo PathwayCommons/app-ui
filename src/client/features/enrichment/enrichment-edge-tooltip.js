a1da193d465d22b62aa0bdbeb135b65db962b7fc
Caitlin <caitlinocallaghan22@gmail.com>
2018-08-16 12:16:37 -0400

    delete commented out code

---
+++ src/client/features/enrichment/enrichment-edge-tooltip.js
@@ -0,0 +1,25 @@
+const React = require('react');
+const h = require('react-hyperscript');
+
+class EnrichmentEdgeTooltip extends React.Component {
+  render(){
+    let { edge }= this.props;
+    let intersection = edge.data('intersection');
+    let intersectionCount = intersection.length;
+
+    return h('div.enrichment-tooltip', [
+      h('div.enrichment-tooltip-header',[
+        // h('h2.enrichment-tooltip-title', title)
+      ]),
+      h('div.enrichment-tooltip-body', [
+        h('div.enrichment-tooltip-field-name', 'Intersection (' + intersectionCount + ')'),
+        h('div.enrichment-tooltip-field-value', intersection.join(', '))
+      ]),
+      h('div.enrichment-tooltip-footer', [
+        // h('a.plain-link', { href: dbInfo.url, target: '_blank', }, dbInfo.name)
+      ])
+    ]);
+  }
+}
+
+module.exports = EnrichmentEdgeTooltip;

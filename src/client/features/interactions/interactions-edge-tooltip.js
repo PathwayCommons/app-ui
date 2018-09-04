1d09314e2e6719392b09141cbed8b7290dd6c418
Dylan Fong <dylanfong.ut@gmail.com>
2018-09-04 16:27:25 -0400

    initial interactions tooltips

--- src/client/features/interactions/interactions-edge-tooltip.js
+++ src/client/features/interactions/interactions-edge-tooltip.js
@@ -5,13 +5,27 @@ class InteractionsEdgeTooltip extends React.Component {
   render(){
     let { edge } = this.props;
     let title = edge.data('id');
-
+    let pubmedIds = edge.data('pubmedIds');
+    let pcIds = edge.data('pcIds');
+    let reactomeIds = edge.data('reactomeIds');
     return h('div.cy-tooltip', [
       h('div.cy-tooltip-header',[
         h('h2.cy-tooltip-title', title)
+      ]),
+      h('div.cy-tooltip-body', [
+        h('div.cy-tooltip-section', [
+          h('div.cy-tooltip-field-name', 'Publications'),
+          h('div.cy-tooltip-field-value', pubmedIds)
+        ]),
+        h('div.cy-tooltip-section', [
+          h('div.cy-tooltip-field-name', 'Pathway Commons Links'),
+          h('div.cy-tooltip-field-value', pcIds)
+        ]),
+        h('div.cy-tooltip-section', [
+          h('div.cy-tooltip-field-name', 'Reactome Links'),
+          h('div.cy-tooltip-field-value', reactomeIds)
+        ])
       ])
-      // h('div.cy-tooltip-body', [
-      // ])
       // h('div.cy-tooltip-footer', [
       // ]),
 
--- src/client/features/interactions/interactions-node-tooltip.js
+++ src/client/features/interactions/interactions-node-tooltip.js
@@ -5,17 +5,24 @@ class InteractionsNodeTooltip extends React.Component {
   render(){
     let { node } = this.props;
     let title = node.data('id');
+    let types = node.data('types');
+    let externalIds = Object.entries(node.data('externalIds'));
 
     return h('div.cy-tooltip', [
       h('div.cy-tooltip-header',[
-        h('h2.cy-tooltip-title', title)
+        h('h2.cy-tooltip-title', title),
+        ...types.map( t => h('div.cy-tooltip-type-chip', t))
       ]),
-      // h('div.cy-tooltip-body', [
-         // TODO: fill with metadata
-      // ]),
-      // h('div.cy-tooltip-footer', [
-         // TODO: fill with metadata
-      // ]),
+      h('div.cy-tooltip-footer', [
+        h('div.cy-tooltip-field-name', [
+          'Links',
+          // h('i.material-icons', 'keyboard_arrow_right')
+        ]),
+        h('div.cy-tooltip-links', externalIds.map( eId => {
+          let [externalDb, externalDbId] = eId;
+          return h('a.plain-link', { href: externalDbId, target: '_blank'}, externalDb);
+        }))
+      ]),
       h('div.cy-tooltip-call-to-action', [
         h('a.cy-tooltip-call-to-action-btn', {
           target: '_blank',

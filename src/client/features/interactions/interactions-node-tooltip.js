2a8598adabd762d01dcfd0b80bd79f97ca97ea27
Dylan Fong <dylanfong.ut@gmail.com>
2018-09-05 13:39:37 -0400

    simplify interactions node tooltips

--- src/client/features/interactions/interactions-node-tooltip.js
+++ src/client/features/interactions/interactions-node-tooltip.js
@@ -6,22 +6,11 @@ class InteractionsNodeTooltip extends React.Component {
     let { node } = this.props;
     let title = node.data('id');
     let types = node.data('types');
-    let externalIds = Object.entries(node.data('externalIds'));
 
     return h('div.cy-tooltip', [
       h('div.cy-tooltip-header',[
         h('h2.cy-tooltip-title', title),
-        ...types.map( t => h('div.cy-tooltip-type-chip', t))
-      ]),
-      h('div.cy-tooltip-footer', [
-        h('div.cy-tooltip-field-name', [
-          'Links',
-          // h('i.material-icons', 'keyboard_arrow_right')
-        ]),
-        h('div.cy-tooltip-links', externalIds.map( eId => {
-          let [externalDb, externalDbId] = eId;
-          return h('a.plain-link', { href: externalDbId, target: '_blank'}, externalDb);
-        }))
+        h('div.cy-tooltip-type-chip', types[0])
       ]),
       h('div.cy-tooltip-call-to-action', [
         h('a.cy-tooltip-call-to-action-btn', {

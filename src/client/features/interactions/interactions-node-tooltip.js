5f209171c4e44c6cbca39e6dd43bae8e37adc862
Dylan Fong <dylanfong.ut@gmail.com>
2018-09-06 15:02:11 -0400

    crude interactions app links - display uniprot, hgnc symbol, and ncbi gene linkouts only and if available - all tooltip links are positioned left to right instead of being space-between positioned

--- src/client/features/interactions/interactions-node-tooltip.js
+++ src/client/features/interactions/interactions-node-tooltip.js
@@ -6,11 +6,41 @@ class InteractionsNodeTooltip extends React.Component {
     let { node } = this.props;
     let title = node.data('id');
     let types = node.data('types');
+    let externalIds = node.data('externalIds');
+
+    let links = [];
+
+    let uniprotId = externalIds['uniprot knowledgebase'];
+    if( uniprotId != null ){
+      links.push({ name: 'Uniprot', url: 'http://identifiers.org/uniprot/' + externalIds['uniprot knowledgebase'] });
+    }
+
+    let hgncSymbolId = externalIds['hgnc symbol'];
+    if( hgncSymbolId != null ){
+      links.push({ name: 'HGNC Symbol', url: 'http://identifiers.org/hgnc.symbol/' + externalIds['hgnc symbol'] });
+    }
+
+    let ncbiGeneId = externalIds['ncbi gene'];
+    if( ncbiGeneId != null ){
+      links.push({ name: 'NCBI Gene', url: 'http://identifiers.org/ncbigene/' + externalIds['ncbi gene'] });
+    }
 
     return h('div.cy-tooltip', [
-      h('div.cy-tooltip-header',[
+      h('div.cy-tooltip-header', [
         h('h2.cy-tooltip-title', title),
         h('div.cy-tooltip-type-chip', types[0])
+      ]),
+      h('div.cy-tooltip-footer', [
+        h('div.cy-tooltip-section', [
+          h('div.cy-tooltip-field-name', [
+            'Links'
+          ]),
+          h('div.cy-tooltip-links', links.map(link => {
+            let { name, url } = link;
+
+            return h('a.plain-link', { href: url, target: '_blank'  }, name);
+          }))
+        ])
       ]),
       h('div.cy-tooltip-call-to-action', [
         h('a.cy-tooltip-call-to-action-btn', {

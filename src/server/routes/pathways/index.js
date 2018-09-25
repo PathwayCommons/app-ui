c83c045be33efa2dbc73e7760ad667915d9d5250
Dylan Fong <dylanfong.ut@gmail.com>
2018-09-25 14:28:10 -0400

    quick and minimal implementation for storing pathways in rethinkdb

--- src/server/routes/pathways/index.js
+++ src/server/routes/pathways/index.js
@@ -1,11 +1,53 @@
+const uuid = require('uuid');
 const express = require('express');
 const router = express.Router();
+
 const { getPathwayJson } = require('./generate-pathway-json');
 
+const db = require('../../db');
+
+let findPathwayByUri = uri => {
+  return db.accessTable('pathways').then( res => {
+    let { table, conn } = res;
+
+    return (
+      table.filter({ uri: uri })
+        .limit(1)
+        .run(conn)
+        .then( cursor => cursor.toArray())
+    );
+  });
+};
+
+let insertPathway = (pathwayJson, uri) => {
+  return db.accessTable('pathways').then( res => {
+    let { table, conn } = res;
+
+    return table.insert({
+      id: uuid(),
+      uri: uri,
+      network: pathwayJson
+    }).run(conn);
+  });
+};
 
 router.get('/', ( req, res ) => {
   let uri = req.query.uri;
-  getPathwayJson( uri ).then( pathwayJson => res.json( pathwayJson ) );
+
+  findPathwayByUri( uri ).then( result => {
+    if( result.length == 0 ){
+      getPathwayJson( uri )
+      .then( pathwayJson => {
+        return insertPathway( pathwayJson ).then( () => pathwayJson );
+      })
+      .then( pathwayJson => {
+        res.json( pathwayJson );
+      });
+    } else {
+      let { network } = result;
+      res.json( network );
+    }
+  });
 });
 
 module.exports = router;

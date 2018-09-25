ed997a72c420fda8b9a60a73f783d6bdab48179d
Dylan Fong <dylanfong.ut@gmail.com>
2018-09-25 15:03:32 -0400

    minor logic fixes

--- src/server/routes/pathways/index.js
+++ src/server/routes/pathways/index.js
@@ -12,40 +12,48 @@ let findPathwayByUri = uri => {
 
     return (
       table.filter({ uri: uri })
-        .limit(1)
-        .run(conn)
-        .then( cursor => cursor.toArray())
+        .limit( 1 )
+        .run( conn )
+        .then( cursor => cursor.toArray() )
+        .then( results => {
+          if( results.length > 0 ){
+            return results[0].network;
+          } else {
+            return null;
+          }
+        } )
     );
   });
 };
 
-let insertPathway = (pathwayJson, uri) => {
+let insertPathway = ( pathwayJson, uri ) => {
   return db.accessTable('pathways').then( res => {
     let { table, conn } = res;
 
-    return table.insert({
-      id: uuid(),
-      uri: uri,
-      network: pathwayJson
-    }).run(conn);
+    return (
+      table.insert({
+        id: uuid(),
+        uri: uri,
+        network: pathwayJson
+      })
+      .run( conn )
+      .then( () => pathwayJson )
+    );
   });
+};
+
+let getAndStorePathway = ( uri ) => {
+  return getPathwayJson( uri ).then( pathwayJson => insertPathway( pathwayJson, uri ) );
 };
 
 router.get('/', ( req, res ) => {
   let uri = req.query.uri;
 
   findPathwayByUri( uri ).then( result => {
-    if( result.length == 0 ){
-      getPathwayJson( uri )
-      .then( pathwayJson => {
-        return insertPathway( pathwayJson ).then( () => pathwayJson );
-      })
-      .then( pathwayJson => {
-        res.json( pathwayJson );
-      });
+    if( result == null ){
+      getAndStorePathway( uri ).then( pathwayJson => res.json( pathwayJson ) );
     } else {
-      let { network } = result;
-      res.json( network );
+      res.json( result );
     }
   });
 });

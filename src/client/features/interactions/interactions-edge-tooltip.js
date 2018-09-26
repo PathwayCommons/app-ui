5132a4e354fb11d787cf0bb2dd521a1275cf085c
Dylan Fong <dylanfong.ut@gmail.com>
2018-09-26 10:31:07 -0400

    fix lint error

--- src/client/features/interactions/interactions-edge-tooltip.js
+++ src/client/features/interactions/interactions-edge-tooltip.js
@@ -24,8 +24,6 @@ class InteractionsEdgeTooltip extends React.Component {
   }
 
   selectEdge(edge){
-    console.log('selectEdge', edge);
-
     this.setState({ selectedEdge: edge });
 
     this.getPublications(edge);

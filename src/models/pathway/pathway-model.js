65dfc76bd2988ae9cac7c1ccae48216ee10dd492
Jeffrey Wong <jvwong@outlook.com>
2018-09-28 14:15:02 -0400

    pathway-model uses datasources

--- src/models/pathway/pathway-model.js
+++ src/models/pathway/pathway-model.js
@@ -1,17 +1,5 @@
 const _ = require('lodash');
-const diceCoefficient = require('dice-coefficient');
-
-let datasourceURLMap = new Map();
-
-datasourceURLMap.set('humancyc','https://humancyc.org'); // HumanCyc
-datasourceURLMap.set('inoh','https://www.ncbi.nlm.nih.gov/pubmed/22120663'); //Integrating Network Objects with Hierarchies
-datasourceURLMap.set('kegg','https://www.kegg.jp'); // KEGG Pathway
-datasourceURLMap.set('pid','https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2686461/'); // NCI Pathway Interaction Database: Pathway
-datasourceURLMap.set('netpath','http://www.netpath.org'); // NetPath
-datasourceURLMap.set('panther','http://www.pantherdb.org'); // PANTHER Pathway
-datasourceURLMap.set('reactome','https://reactome.org'); // Reactome
-datasourceURLMap.set('smpdb','http://smpdb.ca'); // Small Molecule pathway Database
-datasourceURLMap.set('wikipathways','https://www.wikipathways.org'); // WikiPathways
+const datasources = require('../datasources');
 
 // basic pathway model to serve the needs of the pathways view
 // TODO:
@@ -49,20 +37,11 @@ class Pathway {
   }
 
   datasourceUrl(){
-    let datasource = this.datasource();
-    let lowercaseName = datasource.toLowerCase();
-
-    let databaseURL = datasourceURLMap.get(lowercaseName);
-    if( !databaseURL ){
-      let datasourceURLScores = [];
-      datasourceURLMap.forEach( (datasourceURL, datasource) => {
-        datasourceURLScores.push([datasourceURL,diceCoefficient(datasource,lowercaseName)]);
-      });
-      datasourceURLScores.sort( (a,b) => { return b[1] - a[1]; } );
-      databaseURL = datasourceURLScores[0][0];
-    }
-
-    return databaseURL;
+    let datasourceURL = '';
+    const datasource = this.datasource();
+    const databaseInfo = datasources.findByKey(datasource);
+    if ( databaseInfo ) datasourceURL = databaseInfo.homepage;
+    return datasourceURL;
   }
 
   comments(){

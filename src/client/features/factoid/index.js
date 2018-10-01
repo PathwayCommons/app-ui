51f91a4be1fc1429710e9088885f52a970ba1202
Dylan Fong <dylanfong.ut@gmail.com>
2018-10-01 15:07:14 -0400

    factoid react component

---
+++ src/client/features/factoid/index.js
@@ -0,0 +1,14 @@
+const h = require('react-hyperscript');
+const { Component } = require('react');
+
+
+class Factoid extends Component {
+
+  render(){
+    return h('div', 'factoid');
+  }
+}
+
+
+
+module.exports = Factoid;

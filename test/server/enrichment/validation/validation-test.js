9dc3f6e81b2bc302d95cad9a3d06861096b3dd02
Jeffrey Wong <jvwong@outlook.com>
2018-10-05 11:26:41 -0400

    Overhauled validator tests; geneInfo to alias as plain object

--- test/server/enrichment/validation/validation-test.js
+++ test/server/enrichment/validation/validation-test.js
@@ -2,110 +2,70 @@ const chai = require('chai');
 const expect = chai.expect;
 const { validatorGconvert } = require('../../../../src/server/external-services/gprofiler');
 
+const validResult_default = {
+	"unrecognized": [
+		"ATP"
+	],
+	"duplicate": {
+		"HGNC:11998": [
+			"TP53"
+		]
+	},
+	"alias": {
+    "TP53": "HGNC:11998",
+    "ATM": "HGNC:795"
+  }
+};
+
+const validResult_ENSG = {
+	"unrecognized": [
+		"ATP"
+	],
+	"duplicate": {
+		"ENSG00000141510": [
+			"TP53"
+		]
+	},
+	"alias": {
+    "TP53": "ENSG00000141510",
+    "ATM": "ENSG00000149311"
+  }
+};
+
 describe('Test validatorGconvert - Enrichment Validation Service', function() {
-  this.timeout(500000);
-  it('parameters: valid genes', function() {
-    return (validatorGconvert(['TP53', 'ATP', 'ATM', 'TP53'],{})).then(
-      //resolved
-      res => {
-      const result = {
-        "unrecognized": [
-          "ATP"
-        ],
-        "duplicate": {
-          "HGNC:11998": [
-            "TP53"
-          ]
-        },
-        "geneInfo": [
-          {
-            "initialAlias": "TP53",
-            "convertedAlias": "HGNC:11998"
-          },
-          {
-            "initialAlias": "ATM",
-            "convertedAlias": "HGNC:795"
-          }
-        ]
-      };
-      expect(res).to.deep.equal(result);
-      },
-      //rejected
-      (rej) => {
-      console.log("rejected");
-      }
-    );
+
+  it('should return correct results when no additional options are supplied', async () => {
+    const result = await validatorGconvert(["TP53", "ATP", "ATM", "TP53"], {});
+    expect( result ).to.deep.equal( validResult_default );
   });
 
-  it('parameters: valid genes, valid targetDb, valid organism', function() {
-    return (validatorGconvert(['TP53', 'ATP', 'ATM', 'TP53'], {target: "ensg", organism: "hsapiens" })).then(
-      //resolved
-      (res) => {
-      const result = {
-        "unrecognized": [
-          "ATP"
-        ],
-        "duplicate": {
-          "ENSG00000141510": [
-            "TP53"
-          ]
-        },
-        "geneInfo": [
-          {
-            "initialAlias": "TP53",
-            "convertedAlias": "ENSG00000141510"
-          },
-          {
-            "initialAlias": "ATM",
-            "convertedAlias": "ENSG00000149311"
-          }
-        ]
-      };
-      expect(res).to.deep.equal(result);
-      },
-      //rejected
-      (rej) => {
-      console.log("rejected");
-      }
-    );
+  it('should return correct results when options are specified', async () => {
+    const result = await validatorGconvert(["TP53", "ATP", "ATM", "TP53"], {target: "ENSG", organism: "hsapiens" });
+    expect( result ).to.deep.equal( validResult_ENSG );
   });
 
-  it('parameters: valid genes, valid targetDb, invalid organism', function() {
-    return (validatorGconvert(['TP53', 'ATP', 'ATM', 'TP53'], {target: "ENSG", organism: "dog" })).then(
-        //resolved
-        (res) => {
-        console.log("resolved");
-        },
-        //rejected
-        (rej) => {
-        expect(true);
-        }
-    );
+  it('should reject when invalid organism is specified', async () => {
+    try {
+      await validatorGconvert(["TP53", "ATP", "ATM", "TP53"], { organism: "human" });
+    } catch( err ) {
+      expect( err.message ).to.equal( "Invalid organism" );
+    }
   });
 
-  it('parameters: valid genes, invalid targetDb, valid organism', function() {
-    return (validatorGconvert(['TP53', 'ATP', 'ATM', 'TP53'], {target: "layman", organism: "hsapiens" })).then(
-        //resolved
-        (res) =>  {
-        console.log("resolved");
-        },
-        //rejected
-        (rej) => {
-        expect(true);
-        }
-    );
+  it('should reject when invalid target is specified', async () => {
+    try {
+      await validatorGconvert(["TP53", "ATP", "ATM", "TP53"], { target: "nonexistantdatabase" });
+    } catch( err ) {
+      expect( err.message ).to.equal( "Invalid target" );
+    }
   });
 
-  it('parameters: valid genes, invalid targetDb, invalid organism', function() {
-    return (validatorGconvert(['TP53', 'ATP', 'ATM', 'TP53'], {target: "layman", organism: "dog" })).then(
-       //resolved
-       (res) =>  {
-       console.log("resolved");
-       },
-       //rejected
-       (rej) => {
-        expect(true);
-       }
-    );
+  it('should reject when invalid options are specified', async () => {
+    try {
+      await validatorGconvert(["TP53", "ATP", "ATM", "TP53"], { target: "nonexistantdatabase", organism: "human" });
+    } catch( err ) {
+      expect( err ).to.be.instanceOf( Error );
+    }
   });
+
 });

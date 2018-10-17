80cff20543073adfcbd67459f676df23474e8eec
Dylan Fong <dylanfong.ut@gmail.com>
2018-10-17 16:06:20 -0400

    working simplified metadata generation

--- src/server/routes/pathways/generate-pathway-json/biopax-metadata/simple.js
+++ src/server/routes/pathways/generate-pathway-json/biopax-metadata/simple.js
@@ -6,41 +6,86 @@ let extractBiopaxMetadata = biopaxJsonEntry => {
   let displayName = _.get(biopaxJsonEntry, 'displayName', '');
   let synonyms = _.get(biopaxJsonEntry, 'name', []);
   let standardName = _.get(biopaxJsonEntry, 'standardName', '');
-  let db = _.get(biopaxJsonEntry, 'db', null);
-  let er = _.get(biopaxJsonEntry, 'entityReference', null);
+  let comments = _.get(biopaxJsonEntry, 'comment', []);
+  let databaseIds = _.get(biopaxJsonEntry, 'dbIds', []);
 
   return {
+    comments,
     synonyms,
     datasource,
-    type: type.contains(':') ? type.split(':')[1] : type,
+    type: type.includes(':') ? type.split(':')[1] : type,
     standardName,
     displayName,
-    pubmedIds: [],
-    databaseIds: [db, er].filter( el => el !== null )
+    databaseIds
   };
 };
 
-let biopaxMap = biopaxJsonText => {
-  let m = new Map();
+// transform biopaxJsonText into a consolidated js object
+let biopaxText2ElementMap = biopaxJsonText => {
+  let rawMap = new Map();
+  let elementMap = new Map();
+  let xRefMap = new Map();
 
-  JSON.parse(biopaxJsonText)['@graph'].forEach( element => {
+  let biopaxElementGraph = JSON.parse(biopaxJsonText)['@graph'];
+  let externalReferences = [];
+
+  biopaxElementGraph.forEach( element => {
     let uri = element['@id'];
+    rawMap.set(uri, element);
 
-    m.set(uri, element);
+    if( _.has(element, 'xref' ) ){
+      externalReferences.push(element);
+    }
   });
 
-  return m;
+  // extract db ids from specific elements that have 'db' and 'id' fields
+  externalReferences.forEach( element => {
+    let xRefElementId = _.get(element, '@id');
+    let xrefs = [].concat(_.get(element, 'xref', []));
+    let dbIds = {};
+
+    xrefs.forEach( xref => {
+      if( rawMap.has( xref ) ){
+        let { db, id } = rawMap.get( xref );
+
+        if( db != null && id != null ){
+          // e.g.  { 'uniprot': 'Q00987', originalUrl: 'http://identifiers.org/uniprot/Q00987' }
+          if( _.has(dbIds, db) ){
+            dbIds[db] = dbIds[db].concat(id);
+          } else {
+            dbIds[db] = [id];
+          }
+        }
+      }
+    });
+
+    xRefMap.set(xRefElementId, { dbIds } );
+  });
+
+  biopaxElementGraph.forEach( element => {
+    let entityReference = _.get(element, 'entityReference', null);
+    let elementId = _.get(element, '@id');
+    let dbIds = [];
+
+    if( entityReference != null ){
+      dbIds = xRefMap.get( entityReference );
+    }
+    elementMap.set( elementId, _.assign( element, { dbIds } ) );
+  });
+
+  return elementMap;
 };
 
 
 let populateMetadata = ( cyJsonNodes, biopaxJsonText ) => {
-  let bm = biopaxMap( biopaxJsonText );
+  let bm = biopaxText2ElementMap( biopaxJsonText );
   let cyJsonNodeMetadataMap = {};
 
   cyJsonNodes.forEach( node => {
     let nodeId = node.data.id;
-    let altPCId = nodeId.substring(0, nodeId.lastIndexOf('_')); // weird legacy hack to get extra metadata for certain nodes that have PC prefixes
+    let altPCId = nodeId.substring(0, nodeId.lastIndexOf('_'));
 
+    // weird legacy hack to get extra metadata for certain nodes that have PC prefixes
     if( bm.has( nodeId ) ){
       cyJsonNodeMetadataMap[nodeId] = extractBiopaxMetadata( bm.get(nodeId) );
     } else {

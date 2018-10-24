bddf121fc775414b38ea3a610f5e2c5033fee8ee
Dylan Fong <dylanfong.ut@gmail.com>
2018-10-24 11:17:01 -0400

    move generate image json to its own submodule

---
+++ src/server/routes/interactions/generate-interactions-image.js
@@ -0,0 +1,89 @@
+const cytosnap = require('cytosnap');
+
+let snap = cytosnap();
+
+let snapshotInteractionsLayout = network => ({
+  name: 'concentric',
+  minNodeSpacing: network.nodes.length > 5 ? 5 : 50,
+  fit: false
+});
+
+// a cytoscape jsonstylesheet that is compatible with cytosnap
+// it contains fewer properties than the client side interactions app
+let snapshotInteractionsStylesheet = [
+  {
+    selector: 'edge',
+    style: {
+      'opacity': 0.4,
+      'line-color': '#555',
+      'width': 4,
+      'curve-style': 'haystack',
+      'haystack-radius': 0.25
+    }
+  },
+  {
+    selector: 'node',
+    style: {
+      'font-size': 20,
+      'color': '#fff',
+      'background-color': '#555',
+      'text-outline-color': '#555',
+      'text-outline-width': 4,
+      'width': 50,
+      'height': 50,
+      'label': 'data(id)',
+      'text-halign': 'center',
+      'text-valign': 'center'
+    }
+  },
+  {
+    selector: '.Modification',
+    style: {
+      'line-color': '#ffc28b'
+    }
+  },
+  {
+    selector: '.Binding',
+    style: {
+      'line-color': '#8bd8dd'
+    }
+  },
+  {
+    selector: '.Expression',
+    style: {
+      'line-color': '#f4a2a3'
+    }
+  },
+  {
+    selector: '.Other',
+    style: {
+      'line-color': '#949494'
+    }
+  },
+  {
+    selector: 'node[?queried]',
+    style: {
+      'display': 'element',
+      'width': 75,
+      'height': 75
+    }
+  }
+];
+
+
+let generateInteractionsImg = interactionsJson => {
+  return snap.start().then( () => {
+    return snap.shot({
+      elements: interactionsJson,
+      layout: snapshotInteractionsLayout(interactionsJson),
+      style: snapshotInteractionsStylesheet,
+      resolvesTo: 'base64uri',
+      format: 'png',
+      width: 740,
+      height: 740,
+      background: 'transparent'
+    });
+  });
+};
+
+module.exports = { generateInteractionsImg };
--- src/server/routes/interactions/index.js
+++ src/server/routes/interactions/index.js
@@ -1,97 +1,29 @@
 const express = require('express');
 const router = express.Router();
-const cytosnap = require('cytosnap');
 
 const { getInteractionGraphFromPC } = require('./generate-interactions-json');
+const { generateInteractionsImg } = require('./generate-interactions-image');
 
 const logger = require('../../logger');
 
-let snap = cytosnap();
 
 router.get('/', ( req, res ) => {
   let sources = req.query.sources;
 
-  getInteractionGraphFromPC( sources ).then( interactionsJson => res.json(interactionsJson) );
+  getInteractionGraphFromPC( sources )
+  .then( interactionsJson => res.json({ network: interactionsJson }) )
+  .catch( e => {
+    logger.error( e );
+    res.status( 500 ).end( 'Server error' );
+  });
 });
 
 router.get('/image', ( req, res ) => {
   let sources = req.query.sources;
 
-  snap.start().then( () => getInteractionGraphFromPC( sources ) )
-  .then( interactionsJson => {
-    let { network } = interactionsJson;
-    return snap.shot({
-      elements: network,
-      layout: { name: 'concentric', minNodeSpacing: network.nodes.length > 5 ? 5 : 50, fit: false },
-      style: [
-        {
-          selector: 'edge',
-          style: {
-            'opacity': 0.4,
-            'line-color': '#555',
-            'width': 4,
-            'curve-style': 'haystack',
-            'haystack-radius': 0.25
-          }
-        },
-        {
-          selector: 'node',
-          style: {
-            'font-size': 20,
-            'color': '#fff',
-            'background-color': '#555',
-            'text-outline-color': '#555',
-            'text-outline-width': 4,
-            'width': 50,
-            'height': 50,
-            'label': 'data(id)',
-            'text-halign': 'center',
-            'text-valign': 'center'
-          }
-        },
-        {
-          selector: '.Modification',
-          style: {
-            'line-color': '#ffc28b'
-          }
-        },
-        {
-          selector: '.Binding',
-          style: {
-            'line-color': '#8bd8dd'
-          }
-        },
-        {
-          selector: '.Expression',
-          style: {
-            'line-color': '#f4a2a3'
-          }
-        },
-        {
-          selector: '.Other',
-          style: {
-            'line-color': '#949494'
-          }
-        },
-        {
-          selector: 'node[?queried]',
-          style: {
-            'display': 'element',
-            'width': 75,
-            'height': 75
-          }
-        }
-      ],
-      resolvesTo: 'base64uri',
-      format: 'png',
-      width: 740,
-      height: 740,
-      background: 'transparent'
-    });
-  })
-  .then( img => {
-    res.json( { img } );
-  })
+  getInteractionGraphFromPC( sources )
+  .then( generateInteractionsImg )
+  .then( img => res.json( { img } ) )
   .catch( e => {
     logger.error( e );
     res.status( 500 ).end( 'Server error' );

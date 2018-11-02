655bea32bd87048f37f190bdf64b853392ff5fdd
Jeffrey Wong <jvwong@outlook.com>
2018-11-02 09:38:18 -0400

    Removed xref2Uri result from pathway tests

--- test/server/pathways/pathways-network-generation.js
+++ test/server/pathways/pathways-network-generation.js
@@ -6,22 +6,22 @@ const sampleCyjsonData = require('./sample-cyjson-data');
 const sampleMetadataOut = require('./sample-metadata-out.json');
 
 describe('Pathways network generation', function(){
-  // beforeEach(function() {
-  //   global.fetch = () => {
-  //     return new Promise( resolve => {
-  //       resolve({
-  //         ok: true,
-  //         Id: '123',
-  //         text: () => 'http://identifiers.org/mockdb/mockid'
-  //       });
-  //     });
-  //   };
-  // });
+  beforeEach(function() {
+    global.fetch = () => {
+      return new Promise( resolve => {
+        resolve({
+          ok: true,
+          Id: '123',
+          text: () => ''
+        });
+      });
+    };
+  });
 
-  // it('Should return correct metadata from getBiopaxMetadata', async () => {
-  //   let sampleBiopaxData = fs.readFileSync(path.resolve(__dirname, './sample-biopax-data.txt'), 'utf-8');
+  it('Should return correct metadata from getBiopaxMetadata', async () => {
+    let sampleBiopaxData = fs.readFileSync(path.resolve(__dirname, './sample-biopax-data.txt'), 'utf-8');
 
-  //   let result = await getBiopaxMetadata( sampleCyjsonData.nodes, sampleBiopaxData );
-  //   expect( result ).to.deep.equal( sampleMetadataOut );
-  // });
+    let result = await getBiopaxMetadata( sampleCyjsonData.nodes, sampleBiopaxData );
+    expect( result ).to.deep.equal( sampleMetadataOut );
+  });
 });

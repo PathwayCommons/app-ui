b9060df6b210b113069916c76e9dec37c3ca3411
Jeffrey Wong <jvwong@outlook.com>
2018-11-05 10:29:08 -0500

    Trying out a mockFetch module

--- test/server/summary/ncbi/entity-summary-test.js
+++ test/server/summary/ncbi/entity-summary-test.js
@@ -1,32 +1,29 @@
 const chai = require('chai');
 const expect = chai.expect;
+
+const { mockFetch } = require('../../../util');
 const { getEntitySummary } = require('../../../../src/server/external-services/ncbi');
-const config = require('../../../../src/config');
-
-const NCBI_GENE_ESUMMARY_7157 = require('./ncbi-gene-esummary-7157.json');
-const SUMMARY_7157 = require('./summary-7157.json');
-
-const mockJSONFetch = () => {
-    return new Promise( resolve => {
-      resolve({
-        ok: true,
-        json: () => NCBI_GENE_ESUMMARY_7157
-      });
-    });
-  };
+const EMPTY_RESPONSE_DATA_7157 = require('./empty-response-data.json');
+const RESPONSE_DATA_7157 = require('./response-data-7157.json');
+const SUMMARY_7157 = require('./entity-summary-7157.json');
 
 
 describe ('External service: NCBI', function () {
+
   describe ('entityFetch', function () {
 
-    before(function(){
-      global.fetch = mockJSONFetch;
-    });
-
-    it('Should be true', async () => {
-      const result =  await getEntitySummary( ['7157'] );
+    it('Should create a correct EntitySummary from raw JSON', async () => {
+      global.fetch = mockFetch( { json: () => RESPONSE_DATA_7157 } );
+      const result =  await getEntitySummary( [ '7157' ] );
       expect( result ).to.deep.equal( SUMMARY_7157 );
     });
 
+    it('Should return an empty object with no input', async () => {
+      global.fetch = mockFetch( { json: () => EMPTY_RESPONSE_DATA_7157 } );
+      const result =  await getEntitySummary( [ '0000' ] );
+      expect( result ).to.deep.equal( {} );
+    });
+
   });
+
 });

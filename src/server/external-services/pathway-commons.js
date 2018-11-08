eeb5fc07a74207ea31bd3ce29ae7ba9c5b7c5816
Dylan Fong <dylanfong.ut@gmail.com>
2018-11-08 16:13:04 -0500

    improve pathway commons external service code quality

--- src/server/external-services/pathway-commons.js
+++ src/server/external-services/pathway-commons.js
@@ -17,50 +17,60 @@ const fetchOptions = {
   }
 };
 
-const _sanitize = (s) => {
+//Pathway Commons HTTP GET request; options.cmd = 'pc2/get', 'pc2/search', 'pc2/traverse', 'pc2/graph', etc.
+let query = opts => {
+  let queryOpts = _.assign( { user: 'app-ui', cmd: 'pc2/get' }, opts);
+  let { cmd } = queryOpts;
+  let url = config.PC_URL + cmd + '?' + qs.stringify( queryOpts );
+
+  return fetch(url, fetchOptions)
+    .then(res => ( cmd === 'pc2/get' || cmd === 'pc2/graph' ? res.text() : res.json() ) )
+    .catch((e) => {
+      logger.error('query ' + queryOpts + ' failed - ' + e);
+      return null;
+    });
+};
+
+let sanitize = s => {
   // Escape (with '\'), to treat them literally, symbols, such as '*', ':', or space,
   // which otherwise play special roles in a Lucene query string.
   return s.replace(/([!*+\-&|()[\]{}^~?:/\\"\s])/g, '\\$1');
 };
 
-const _processPhrase = (phrase) => {
-  return validatorGconvert(phrase.split(' '),{}).then(result => {
+
+// recognize biological entities from an input string
+let extractEntityIds = inputString => {
+  let tokens = inputString.split(' ');
+
+  return validatorGconvert( tokens ).then( result => {
     let { unrecognized, alias  } = result;
-    const entities = _.keys( alias ).map( initialAlias =>'xrefid:' + _sanitize( initialAlias.toUpperCase()) );
-    const otherIds = unrecognized.map(id=>{
-      id=id.toUpperCase();
-      const recognized = /^SMP\d{5}$/.test(id) // check for a smpdb or chebi id
-        ||/^CHEBI:\d+$/.test(id) && (id.length <= ("CHEBI:".length + 6));
-      const sanitized = _sanitize(id);
+    let entities = _.keys( alias ).map( initialAlias => 'xrefid:' + sanitize( initialAlias.toUpperCase() ) );
+
+    let otherIds = unrecognized.map( id => {
+      id = id.toUpperCase();
+      let isChebiId = /^CHEBI:\d+$/.test( id );
+      let isSmpdbId = /^SMP\d{5}$/.test( id );
+      let recognized = isSmpdbId || isChebiId && ( id.length <= ("CHEBI:".length + 6) );
+      let sanitized = sanitize(id);
+
       return recognized ? ( 'xrefid:' + sanitized ) : ( 'name:' + '*' + sanitized + '*' );
     });
+
     return entities.concat(otherIds);
   });
 };
 
-const _processQueryString = async (inputString) => {
-  const keywords = await _processPhrase(inputString);
-  const phrase = _sanitize(inputString);
-  // return three search query candidates: the first one is the fastest, the last - slowest
-  return [
-    '(name:' + phrase + ') OR (' + 'name:*' + phrase + '*) OR (' + keywords.join(' AND ') + ')',
-    '(' + keywords.join(' OR ') + ')',
-    inputString //"as is" (won't additionally escape Lucene query syntax, spaces, etc.)
-  ];
-};
+// generate three search query candidates: the first one is the fastest, the last - slowest
+let generateSearchQueries = inputString => {
+  let phrase = sanitize( inputString );
 
-//Pathway Commons HTTP GET request; options.cmd = 'pc2/get', 'pc2/search', 'pc2/traverse', 'pc2/graph', etc.
-const query = async (queryObj) => {
-  queryObj.user = 'app-ui';
-  let cmd = queryObj.cmd || 'pc2/get';
-  //TODO: (not critical) client app's sends useless parameters to the PC server: cmd, lt, gt
-  const url = config.PC_URL + cmd + '?' + qs.stringify(queryObj);
-  return fetch(url, fetchOptions)
-    .then(res => (cmd=='pc2/get'||cmd=='pc2/graph')?res.text():res.json())
-    .catch((e) => {
-      logger.error('query ' + queryObj + ' failed - ' + e);
-      return null;
-    });
+  return extractEntityIds( inputString ).then( entities => {
+    return [
+      '(name:' + phrase + ') OR (' + 'name:*' + phrase + '*) OR (' + entities.join(' AND ') + ')',
+      '(' + entities.join(' OR ') + ')',
+      inputString //"as is" (won't additionally escape Lucene query syntax, spaces, etc.)
+    ];
+  });
 };
 
 // A fine-tuned PC search to improve relevance of full-text search and filter out unwanted hits.
@@ -69,22 +79,22 @@ const query = async (queryObj) => {
 //  - type: BioPAX type to match/filter by
 //  - lt: max graph size result returned
 //  - gt: min graph size result returned
-const _search = async (args) => {
-  const minSize = args.gt || 0;
-  const maxSize = args.lt || 250;
-  //analyse the input string, generate specific (lucene) search sub-queries
-  const queryString = args.q.trim();
-  const queries = await _processQueryString(queryString);
-  for (let q of queries) {
-    args.cmd = 'pc2/search'; //PC command
-    args.q = q; //override initial query.q string with the sub-query q
-    const searchResult = await query(args); //up to 100 hits at once; if we need more, then must use 'page' parameter...
-    const searchSuccess = searchResult != null;
-    if (searchSuccess && searchResult.searchHit.length > 0) {
-      const filteredResults = searchResult.searchHit.filter(hit => {
-        const size = hit.numParticipants ? hit.numParticipants : 0;
+let search = async opts => {
+  let { gt:minSize = 0, lt:maxSize = 250, q } = opts;
+  let queryStrings = await generateSearchQueries( q.trim() );
+
+  for( let queryString of queryStrings ) {
+    let queryOpts = _.assign( opts, { cmd: 'pc2/search', q: queryString } );
+    let searchResult = await query( queryOpts );
+    let searchResults = _.get( searchResult, 'searchHit', []);
+
+    if( searchResults.length > 0 ){
+      let filteredResults = searchResults.filter( result => {
+        let size = _.get( result, 'numParticipants', 0);
+
         return minSize < size && size < maxSize;
       });
+
       if (filteredResults.length > 0) {
         return filteredResults;
       }
@@ -94,32 +104,21 @@ const _search = async (args) => {
   return [];
 };
 
-const sifGraph = async ( queryObj ) => {
-  let path;
-  const defaults = {
+const sifGraph = opts => {
+  let hasMultipleSources = _.get(opts, 'source', []).length > 1;
+  let sifGraphType = hasMultipleSources ? 'pathsbetween' : 'neighborhood';
+  let queryOpts = Object.assign( {}, opts, {
     limit: 1,
-    pattern: ['CONTROLS_STATE_CHANGE_OF','CONTROLS_TRANSPORT_OF','CONTROLS_EXPRESSION_OF','CATALYSIS_PRECEDES','INTERACTS_WITH']
-  };
-  const params = _.assign(defaults, queryObj);
+    pattern: ['CONTROLS_STATE_CHANGE_OF','CONTROLS_TRANSPORT_OF','CONTROLS_EXPRESSION_OF','CATALYSIS_PRECEDES','INTERACTS_WITH'],
+    directed: hasMultipleSources ? 'false' : undefined,
+    direction: hasMultipleSources ? undefined : 'UNDIRECTED'
+  });
+  let url = config.PC_URL + 'sifgraph/v1/' + sifGraphType + '?' + qs.stringify( queryOpts );
 
-  if ( params.source.length > 1 ){
-    path = 'pathsbetween';
-    params.directed = 'false';
-  } else {
-    path = 'neighborhood';
-    params.direction = 'UNDIRECTED';
-  }
-
-  const url = config.PC_URL + 'sifgraph/v1/' + path + '?' + qs.stringify(params);
-  return fetch(url, {
-    method: 'GET',
-    headers: {
-      'Accept': 'text/plain'
-    }
-  })
+  return fetch( url, { method: 'GET', headers: { 'Accept': 'text/plain' } } )
   .then( res => res.text() )
   .catch( e => {
-    logger.error('sifGraph ' + queryObj + ' failed - ' + e);
+    logger.error( 'sifGraph ' + opts + ' failed - ' + e );
     throw e;
   });
 };
@@ -183,6 +182,5 @@ const xref2Uri =  ( name, localId ) => {
     }) );
 };
 
-const search = _.memoize(_search, query => JSON.stringify(query));
 
-module.exports = { query, search, sifGraph, xref2Uri };
+module.exports = { query, search: _.memoize( search, query => JSON.stringify( query ) ), sifGraph, xref2Uri };

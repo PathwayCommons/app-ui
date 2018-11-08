4524a0161d1bc7fd7a8cc9a6fe9936ea6d0e7d76
Jeffrey Wong <jvwong@outlook.com>
2018-11-08 16:31:36 -0500

    UniProt service tests

--- src/server/external-services/uniprot.js
+++ src/server/external-services/uniprot.js
@@ -1,5 +1,5 @@
 const { fetch } = require('../../util');
-const { UNIPROT_API_BASE_URL } = require('../../config');
+const { UNIPROT_API_BASE_URL, NS_GENECARDS, NS_HGNC_SYMBOL, NS_NCBI_GENE, NS_UNIPROT, IDENTIFIERS_URL } = require('../../config');
 const _ = require('lodash');
 const { EntitySummary, DATASOURCES } = require('../../models/entity/summary');
 const logger = require('../logger');
@@ -18,9 +18,11 @@ const fetchByAccessions = ( accessions ) => {
     });
 };
 
+const createUri = ( namespace, localId ) => IDENTIFIERS_URL + '/' + namespace + '/' + localId;
+
 const getEntitySummary = async ( accessions ) => {
 
-  const summary = {};
+  const summary = [];
   if ( _.isEmpty( accessions ) ) return summary;
 
   const results = await fetchByAccessions( accessions );
@@ -28,29 +30,47 @@ const getEntitySummary = async ( accessions ) => {
 
   results.forEach( doc => {
 
-    // Fetch external database links first
-    const xref = {};
+    const accession = _.get( doc, 'accession', '');
+
+    // Create external database links first
+    const xrefLinks = [{
+      "namespace": NS_UNIPROT,
+      "uri": createUri( NS_UNIPROT, accession )
+    }];
     doc.dbReferences.forEach( xrf => {
-      if ( xrf.type === 'GeneID' ) {
-        xref[DATASOURCES.NCBIGENE] = _.get( xrf, 'id', '');
-      }
-      if ( xrf.type === 'HGNC' ) {
-        xref[DATASOURCES.HGNC] = _.get( xrf, "properties['gene designation']");
-        xref[DATASOURCES.GENECARDS] = _.get( xrf, "properties['gene designation']");
+      switch ( xrf.type ){
+        case 'HGNC':
+          xrefLinks.push({
+            "namespace": NS_GENECARDS,
+            "uri": createUri( NS_GENECARDS, _.get( xrf, "properties['gene designation']" ) )
+          });
+          xrefLinks.push({
+            "namespace": NS_HGNC_SYMBOL,
+            "uri": createUri( NS_HGNC_SYMBOL, _.get( xrf, "properties['gene designation']" ) )
+          });
+          break;
+        case 'GeneID':
+          xrefLinks.push({
+            "namespace": NS_NCBI_GENE,
+            "uri": createUri( NS_NCBI_GENE, _.get( xrf, 'id', '') )
+          });
+          break;
       }
     });
-    const accession = _.get( doc, 'accession', '');
     const eSummary = new EntitySummary({
-      dataSource: DATASOURCES.UNIPROT,
+      namespace: NS_UNIPROT,
       displayName: _.get( doc, 'protein.recommendedName.fullName.value', ''),
       localID: accession,
       description: _.get( doc, 'comments[0].text[0].value', ''),
       aliases: _.get( doc, 'protein.alternativeName', []).map( elt =>  _.get( elt, 'fullName.value') ),
       aliasIds: _.get( doc, 'protein.recommendedName.shortName', []).map( elt =>  _.get( elt, 'value') ),
-      xref: xref
+      xrefLinks: xrefLinks
     });
 
-    return summary[ accession ] = eSummary;
+    summary.push({
+      "query": accession,
+      "entitySummary": eSummary
+    });
   });
 
   return summary;

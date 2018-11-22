8a8367a7d81b586884eacf135a88b8b38e2bca1c
Dylan Fong <dylanfong.ut@gmail.com>
2018-11-22 15:31:31 -0500

    enrichment map tooltips more consistent with other app tooltips - 'search pathway' title changed to 'find related pathways' - each gene in genelist has a linkout to search - 'links' section added to the enrichment tooltips
    refs #1153

--- src/client/features/enrichment/enrichment-tooltip.js
+++ src/client/features/enrichment/enrichment-tooltip.js
@@ -50,33 +50,49 @@ class EnrichmentTooltip extends React.Component {
     else if( id.includes('REAC') ) dbInfo = {name: 'Reactome', url:'http://identifiers.org/reactome/' + id.replace("REAC:", "R-HSA-") };
 
     return h('div.cy-tooltip', [
-      h('div.cy-tooltip-header',[
-        h('h2.cy-tooltip-title', title)
-      ]),
-      h('div.cy-tooltip-body', [
-        h('div.cy-tooltip-section', [
-          h('div.cy-tooltip-field-name', 'Pathway Overview'),
-          h('div.cy-tooltip-field-value', description)
+      h('div.cy-tooltip-content', [
+        h('div.cy-tooltip-header',[
+          h('h2.cy-tooltip-title', title)
         ]),
-        h('div.cy-tooltip-section', [
-          h('div.cy-tooltip-field-name', 'Genes Shared with Entered List (' + sharedGeneCount + ')'),
-          h('div.cy-tooltip-field-value', sharedGeneList.join(', ')),
+        h('div.cy-tooltip-body', [
+          h('div.cy-tooltip-section', [
+            h('div.cy-tooltip-field-name', 'Pathway Overview'),
+            h('div.cy-tooltip-field-value', description)
+          ]),
+          h('div.cy-tooltip-section', [
+            h('div.cy-tooltip-field-name', 'Genes Shared with Entered List (' + sharedGeneCount + ')'),
+            h('div.cy-tooltip-field-value', sharedGeneList.join(', ')),
+          ]),
+          h('div.cy-tooltip-section', [
+            h('div.cy-tooltip-field-name', 'Genes in Pathway (' + geneCount + ')'),
+            h('div.cy-tooltip-field-value', [
+              h('div.enrichment-tooltip-gene-list', geneSet.map( ( gene, index ) => {
+                return h('div.enrichment-tooltip-gene-link', [
+                  h('a.plain-link', { target: '_blank', href: '/search?q=' + gene }, gene),
+                  index === geneSet.length - 1 ? '': ','
+                ]);
+              }))
+            ])
+          ])
         ]),
-        h('div.cy-tooltip-section', [
-          h('div.cy-tooltip-field-name', 'Genes in Pathway (' + geneCount + ')'),
-          h('div.cy-tooltip-field-value', geneSet.join(', '))
+        h('div.cy-tooltip-footer', [
+          h('div.cy-tooltip-section', [
+            h('div.cy-tooltip-field-name', [
+              'Links'
+            ])
+          ]),
+          h('div.cy-tooltip-links', [
+            h('a.plain-link', { href: dbInfo.url, target: '_blank', }, dbInfo.name)
+          ])
+        ]),
+        h('div.cy-tooltip-call-to-action', [
+          h('a', {
+            target: '_blank',
+            href: '/search?q=' + title
+            }, [
+              h('button.call-to-action', 'Find Related Pathways')
+          ])
         ])
-      ]),
-      h('div.cy-tooltip-footer', [
-        h('a.plain-link', { href: dbInfo.url, target: '_blank', }, dbInfo.name)
-      ]),
-      h('div.cy-tooltip-call-to-action', [
-        h('a.call-to-action', {
-          target: '_blank',
-          href: '/search?q=' + title
-          },
-          `SEARCH PATHWAY`
-        )
       ])
     ]);
   }

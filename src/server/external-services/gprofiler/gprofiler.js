6f20f2d502099a540fc11b5727d3fc807a4e752c
Dylan Fong <dylanfong.ut@gmail.com>
2018-11-26 17:45:22 -0500

    clean up gprofiler response parsing
    refs #1153 clean up enrichment services

--- src/server/external-services/gprofiler/gprofiler.js
+++ src/server/external-services/gprofiler/gprofiler.js
@@ -5,40 +5,58 @@ const qs = require('query-string');
 const cleanUpEntrez = require('./clean-up-entrez');
 const { GPROFILER_URL } = require('../../../config');
 
-const defaultSetting = {
-  "output": "mini",
-  "organism": "hsapiens",
-  "significant": 0,
-  "sort_by_structure": 1,
-  "ordered_query": 0,
-  "as_ranges": 0,
-  "no_iea": 1,
-  "underrep": 0,
-  "hierfiltering": "none",
-  "user_thr": 0.05,
-  "min_set_size": 5,
-  "max_set_size": 200,
-  "threshold_algo": "fdr",
-  "domain_size_type": "annotated",
-  "custbg": [],
-  "sf_GO:BP": 1,
-  "sf_REAC": 1,
-  "prefix": 'ENTREZGENE_ACC'
+const GPROFILER_DEFAULT_OPTS = {
+  'output': 'mini',
+  'organism': 'hsapiens',
+  'significant': 0,
+  'sort_by_structure': 1,
+  'ordered_query': 0,
+  'as_ranges': 0,
+  'no_iea': 1,
+  'underrep': 0,
+  'hierfiltering': 'none',
+  'user_thr': 0.05,
+  'min_set_size': 5,
+  'max_set_size': 200,
+  'threshold_algo': 'fdr',
+  'domain_size_type': 'annotated',
+  'custbg': [],
+  'sf_GO:BP': 1,
+  'sf_REAC': 1,
+  'prefix': 'ENTREZGENE_ACC'
 };
 
 // parseGProfilerResponse(gProfilerResponse) takes the text response
 // from gProfiler gProfilerResponse and parses it into JSON format
-const parseGProfilerResponse = (gProfilerResponse) => {
-  let lines = _.map(gProfilerResponse.split('\n'), line => {
-    if (line.substring(0, 1) === '#') {
-      return '';
-    }
+const parseGProfilerResponse = gProfilerResponse => {
+  let lines = gProfilerResponse.split('\n').map( line => {
+    if( line.substring(0, 1) === '#' ){ return ''; }
     return line;
   });
-  lines = _.compact(lines);
-  return _.map(lines, line => {
-    return line.split('\t');
+
+  let elements = _.compact(lines).map( line => line.split('\t') );
+
+  let pathwayInfo = {};
+  let P_VALUE_INDEX = 2;
+  let PATHWAY_ID_INDEX = 8;
+  let DESCRIPTION_INDEX = 11;
+  let GENE_INTERSECTION_LIST_INDEX = 13;
+
+
+  elements.forEach( ele => {
+    let pathwayId = ele[PATHWAY_ID_INDEX];
+    let pValue = ele[P_VALUE_INDEX];
+    let description = ele[DESCRIPTION_INDEX];
+    let geneIntersectionList = ele[GENE_INTERSECTION_LIST_INDEX].split(',').map( gene => cleanUpEntrez( gene ) );
+
+    pathwayInfo[pathwayId] = {
+      p_value: pValue,
+      description,
+      intersection: geneIntersectionList
+    };
   });
+
+  return { pathwayInfo };
 };
 
 
@@ -56,55 +74,39 @@ const enrichment = (query, userSetting) => {
   });
 
   return new Promise((resolve, reject) => {
-    let formData = _.assign({}, defaultSetting, JSON.parse(JSON.stringify(userSetting)), { query: query });
-    const queryVal = formData.query;
-    const minSetSizeVal = formData.min_set_size;
-    const maxSetSizeVal = formData.max_set_size;
-    const backgroundGenesVal = formData.custbg;
+    let formData = _.assign({}, GPROFILER_DEFAULT_OPTS, JSON.parse(JSON.stringify(userSetting)), { query: query });
+    let {
+      query: queryVal,
+      min_set_size: minSetSize,
+      max_set_size: maxSetSize,
+      custbg: backgroundGenes
+    } = formData;
+
     if (!Array.isArray(queryVal)) {
       reject(new Error('ERROR: genes should be an array'));
     }
-    formData.query = query.join(" ");
+    formData.query = query.join(' ');
     if (typeof(formData.min_set_size) != 'number') {
       reject(new Error('ERROR: minSetSize should be a number'));
     }
-    if (minSetSizeVal < 0) {
+    if (minSetSize < 0) {
       reject(new Error('ERROR: minSetSize should be >= 0'));
     }
     if (typeof(formData.max_set_size) != 'number') {
       reject(new Error('ERROR: maxSetSize should be a number'));
     }
-    if (maxSetSizeVal < minSetSizeVal) {
+    if (maxSetSize < minSetSize) {
       reject(new Error('ERROR: maxSetSize should be >= minSetSize'));
     }
-    if (!Array.isArray(backgroundGenesVal)) {
+    if (!Array.isArray(backgroundGenes)) {
       reject(new Error('ERROR: backgroundGenes should be an array'));
     }
-    formData.custbg = backgroundGenesVal.join(" ");
+    formData.custbg = backgroundGenes.join(' ');
 
-    fetch(GPROFILER_URL, {
-      method: 'post',
-      body: qs.stringify(formData)
-    }).then(gProfilerResponse => gProfilerResponse.text())
-      .then(body => {
-        const responseInfo = parseGProfilerResponse(body);
-        let ret = {};
-        const pValueIndex = 2;
-        const termIdIndex = 8;
-        const tNameIndex = 11;
-        const qAndTListIndex = 13;
-        ret.pathwayInfo = {};
-        _.forEach(responseInfo, elem => {
-          ret.pathwayInfo[elem[termIdIndex]] = {
-            "p_value": Number(elem[pValueIndex]),
-            "description": elem[tNameIndex].trim(),
-            "intersection": _.map(elem[qAndTListIndex].split(','), gene => {
-              return cleanUpEntrez(gene);
-            })
-          };
-        });
-        resolve(ret);
-      });
+    fetch(GPROFILER_URL, { method: 'post', body: qs.stringify(formData)})
+      .then( res => res.text() )
+      .then( gprofilerRes =>  parseGProfilerResponse( gprofilerRes ) )
+      .then( pathwayInfo => resolve( pathwayInfo ) );
   });
 };
 

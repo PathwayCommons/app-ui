5395e4e2afdb436227cc3e70c3e7cabe55720135
Dylan Fong <dylanfong.ut@gmail.com>
2018-11-27 12:02:43 -0500

    clean up 'pathway-table' module

--- src/server/routes/enrichment/visualization/pathway-table/index.js
+++ src/server/routes/enrichment/visualization/pathway-table/index.js
@@ -1,4 +1,29 @@
-const { pathwayInfoTable } = require('./preprocess');
+const fs = require('fs');
+const path = require('path');
+const gmtPathwayData = fs.readFileSync(path.resolve(__dirname, 'hsapiens.pathways.NAME.gmt')).toString('utf8');
+
+// pathwayInfoTable is map where the keys are GO/REACTOME pathway identifiers
+// and values are description and geneset
+const pathwayInfoTable = new Map();
+
+gmtPathwayData.split('\n').forEach( pathwayInfoLine => {
+  let pathwayInfoTokens = pathwayInfoLine.split('\t');
+  let PATHWAY_ID_INDEX = 0;
+  let DESCRIPTION_INDEX = 1;
+  let GENE_LIST_START_INDEX = 2;
+
+  let pathwayId = pathwayInfoTokens[PATHWAY_ID_INDEX];
+  let description = pathwayInfoTokens[DESCRIPTION_INDEX];
+  let geneset = [];
+
+  for( let i = GENE_LIST_START_INDEX; i < pathwayInfoTokens.length; ++i ){
+    geneset.push(pathwayInfoTokens[i]);
+  }
+
+  pathwayInfoTable.set(pathwayId, { description, geneset } );
+} );
+
+pathwayInfoTable.delete('');
 
 
 module.exports = { pathwayInfoTable };

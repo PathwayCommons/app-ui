038c7660e1ef9b63ac7f95b2a1a4c079a48ab9fe
Dylan Fong <dylanfong.ut@gmail.com>
2018-12-05 17:53:48 -0500

    remove absoluteUrl nonsense

--- src/client/services/server-api/index.js
+++ src/client/services/server-api/index.js
@@ -4,10 +4,6 @@ const _ = require('lodash');
 const { PC_URL } = require('../../../config');
 const { fetch } = require('../../../util');
 
-let absoluteURL = (href) => {
-  return ( location.origin + href) ;
-};
-
 const defaultFetchOpts = {
   headers: {
     'Content-Type': 'application/json',
@@ -37,7 +33,7 @@ const ServerAPI = {
   },
 
   getPathway(uri) {
-    let url = absoluteURL(`/api/pathways?${ qs.stringify({ uri }) }`);
+    let url = `/api/pathways?${ qs.stringify({ uri }) }`;
     return (
       fetch(url, defaultFetchOpts)
         .then(res =>  res.json())
@@ -57,7 +53,7 @@ const ServerAPI = {
   },
 
   getFactoid(id) {
-    let url = absoluteURL(`/api/factoids/${ id }`);
+    let url = `/api/factoids/${ id }`;
     return (
       fetch(url, defaultFetchOpts)
         .then(res =>  res.json())
@@ -71,7 +67,7 @@ const ServerAPI = {
 
   getInteractionGraph(sources) {
     return (
-      fetch(absoluteURL(`/api/interactions?${qs.stringify(sources)}`), defaultFetchOpts)
+      fetch(`/api/interactions?${qs.stringify(sources)}`, defaultFetchOpts)
        .then( res => res.json())
     );
   },
@@ -120,11 +116,19 @@ const ServerAPI = {
     if (/^((uniprot|hgnc):\w+|ncbi:[0-9]+)$/i.test(queryClone.q)) {
       queryClone.q=queryClone.q.replace(/^(uniprot|ncbi|hgnc):/i,"");
     }
-    return fetch(absoluteURL(`/api/pc/search?${qs.stringify(queryClone)}`), defaultFetchOpts).then(res => res.json());
+    return fetch(`/api/pc/search?${qs.stringify(queryClone)}`, defaultFetchOpts).then(res => res.json());
+  },
+
+  s(query){
+    const queryClone=_.assign({},query);
+    if (/^((uniprot|hgnc):\w+|ncbi:[0-9]+)$/i.test(queryClone.q)) {
+      queryClone.q=queryClone.q.replace(/^(uniprot|ncbi|hgnc):/i,"");
+    }
+    return fetch(`/api/search?${qs.stringify(queryClone)}`, defaultFetchOpts).then(res => res.json());
   },
 
   enrichmentAPI(query, type){
-    return fetch(absoluteURL(`/api/enrichment/${type}`), {
+    return fetch(`/api/enrichment/${type}`, {
       method:'POST',
       headers: {
         'Accept': 'application/json',

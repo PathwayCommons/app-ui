c62151bd70be5c3e9e4b345fff15bbacd0cac9a4
Dylan Fong <dylanfong.ut@gmail.com>
2018-12-05 12:54:14 -0500

    don't create console, debug, and warn files when the app is run in production refs #1179

--- src/server/logger.js
+++ src/server/logger.js
@@ -1,13 +1,21 @@
 let winston = require('winston');
-let config = require('../config');
+let { NODE_ENV, LOG_LEVEL } = require('../config');
+
+
+let transports = [
+  new (winston.transports.File)({ name: 'error', filename: 'error.log', level: 'error' })
+];
+
+if( NODE_ENV !== 'production' ){
+  transports = transports.concat([
+    new (winston.transports.Console)({ level: LOG_LEVEL }),
+    new (winston.transports.File)({ name: 'debug', filename: 'debug.log', level: 'debug' }),
+    new (winston.transports.File)({ name: 'warn', filename: 'warn.log', level: 'warn' })
+  ]);
+}
 
 let logger = new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)({ level: config.LOG_LEVEL }),
-    new (winston.transports.File)({ name: 'debug', filename: 'debug.log', level: 'debug' }),
-    new (winston.transports.File)({ name: 'warn', filename: 'warn.log', level: 'warn' }),
-    new (winston.transports.File)({ name: 'error', filename: 'error.log', level: 'error' })
-  ]
+  transports
 });
 
 logger.cli();

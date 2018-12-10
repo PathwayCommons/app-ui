015fc055ea989fa9feac76266c3a838ad07d050b
Dylan Fong <dylanfong.ut@gmail.com>
2018-12-10 11:51:04 -0500

    get geneSymbol from the gene response info

--- src/client/features/search/gene-results-view.js
+++ src/client/features/search/gene-results-view.js
@@ -13,18 +13,11 @@ const SUPPORTED_COLLECTIONS = new Map([
   [NS_UNIPROT, 'UniProt']
 ]);
 
-const getHgncFromXref = xrefLinks => {
-  let symbol;
-  const hgncXrefLink = _.find( xrefLinks, link  => link.namespace === NS_HGNC_SYMBOL );
-  if( hgncXrefLink ) symbol = _.last( _.compact( hgncXrefLink.uri.split('/') ) );
-  return symbol;
-};
-
 class EntitySummaryBox extends React.Component {
   render(){
-    let { summary } = this.props;
+    let { geneInfo } = this.props;
+    let { geneSymbol, summary } = geneInfo;
     let { displayName, xrefLinks } = summary;
-    const hgncSymbol = getHgncFromXref( xrefLinks );
 
     // sometimes duplicated namespace/uri pairs are received e.g. uniprot/tp53 twice
     let sortedLinks = _.uniqWith(xrefLinks.sort( (p1, p2) => p1.namespace > p2.namespace ? 1: -1 ), (p1, p2) => p1.namespace === p2.namespace )
@@ -33,7 +26,7 @@ class EntitySummaryBox extends React.Component {
         return (
       h('div.entity-summary-box', [
           h('h5.entity-subtitle', displayName),
-          h('h3.entity-title', hgncSymbol),
+          h('h3.entity-title', geneSymbol),
         h('div.entity-links-container', [
           ...sortedLinks
         ])
@@ -62,7 +55,7 @@ class GeneResultsView extends React.Component {
       let linkPath = '/interactions';
       let imageClass = 'interactions-logo';
       let title = 'Interactions';
-      
+
       if( sources.length === 1 ){
         label = `View iteractions between ${sources.slice(0, sources.length - 1).join(', ')} and ${sources.slice(-1)}`;
       }
@@ -71,7 +64,7 @@ class GeneResultsView extends React.Component {
     };
 
     let { geneResults } = this.props;
-    let sources = geneResults.map( summaryResult => getHgncFromXref( _.get( summaryResult, ['summary', 'xrefLinks'] ) ) );
+    let sources = geneResults.map( geneInfo => geneInfo.geneSymbol );
 
     return sources.length < 4 ? createInteractionsAppInfo(sources) : createEnrichmentAppInfo(sources);
   }
@@ -79,15 +72,15 @@ class GeneResultsView extends React.Component {
   render(){
     let { geneResults } = this.props;
     let { label, title, linkPath, description, imageClass } = this.determineAppLinkout();
-    let sources = geneResults.map( summaryResult => getHgncFromXref( _.get( summaryResult, ['summary', 'xrefLinks'] ) ) );
+    let sources = geneResults.map( geneInfo => geneInfo.geneSymbol );
 
     return h('div.search-genes-results', [
       h('h3.search-genes-header', `Genes (${geneResults.length})`),
       h('div.search-genes-info-panel', [
         h('div.search-genes-list', [
-          ...geneResults.map( s => {
+          ...geneResults.map( geneInfo => {
             return h('div.card', [
-              h(EntitySummaryBox, { summary: _.get(s, `summary`) } )
+              h(EntitySummaryBox, { geneInfo } )
             ]);
           })
         ]),

06544d27bbc7a3e38ace890d7160099e4c18b82e
Dylan Fong <dylanfong.ut@gmail.com>
2018-12-17 15:20:24 -0500

    lazy load interactions node info refs #1234

--- src/client/features/interactions/interactions-node-tooltip.js
+++ src/client/features/interactions/interactions-node-tooltip.js
@@ -1,13 +1,54 @@
 const React = require('react');
 const h = require('react-hyperscript');
+const _ = require('lodash');
 
+const { NS_GENECARDS, NS_NCBI_GENE, NS_HGNC_SYMBOL, NS_UNIPROT } = require('../../../config');
+
+const { ServerAPI } = require('../../services');
 class InteractionsNodeTooltip extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      entitySummary: {},
+      entitySummaryLoaded: false
+    };
+  }
+
+  componentDidMount(){
+    let { node } = this.props;
+    let query = node.data('id');
+    ServerAPI.entitySummaryQuery( query ).then( summary => {
+      this.setState({ entitySummary: summary, entitySummaryLoaded: true });
+    } );
+  }
+
   render(){
     let { node } = this.props;
+    let { entitySummary, entitySummaryLoaded } = this.state;
+    let xrefLinks = _.get(entitySummary, '0.summary.xrefLinks', []);
     let title = node.data('id');
     let links = [];
 
-    links.push({ name: 'HGNC', url: 'http://identifiers.org/hgnc.symbol/' + node.data('id') });
+    if( !entitySummaryLoaded ){
+      return h('div.cy-tooltip', [
+        h('div.cy-tooltip-content', [
+          h('div.cy-tooltip-header',[
+            h('h2.cy-tooltip-title', 'Loading...')
+          ]),
+          h('div.cy-tooltip-body', [
+            h('div.cy-tooltip-loading-section', [
+              h('i.icon.icon-spinner')
+            ])
+          ])
+        ])
+      ]);
+    }
+
+    links.push({ name: 'HGNC', url: xrefLinks.find( link => link.namespace === NS_HGNC_SYMBOL ).uri });
+    links.push({ name: 'UniProt', url: xrefLinks.find( link => link.namespace === NS_UNIPROT ).uri });
+    links.push({ name: 'NCBI Gene', url: xrefLinks.find( link => link.namespace === NS_NCBI_GENE ).uri });
+    links.push({ name: 'Gene Cards', url: xrefLinks.find( link => link.namespace === NS_GENECARDS ).uri });
 
     return h('div.cy-tooltip', [
       h('div.cy-tooltip-header', [

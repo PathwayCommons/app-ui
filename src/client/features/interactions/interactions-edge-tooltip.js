8caeab3ca73d8c1ecd3c5ac384ac45391d3b0206
d2fong <d2fong@uwaterloo.ca>
2018-12-26 11:35:28 -0500

    fix interactions edge render logic when there is only one edge refs #1259

--- src/client/features/interactions/interactions-edge-tooltip.js
+++ src/client/features/interactions/interactions-edge-tooltip.js
@@ -20,7 +20,7 @@ class InteractionsEdgeTooltip extends React.Component {
     let pubmedIds = edge.data('pubmedIds');
 
     this.setState({ publicationsLoaded: false }, () => {
-      ServerAPI.getPubmedPublications(pubmedIds).then( publications => {
+    ServerAPI.getPubmedPublications(pubmedIds).then( publications => {
         this.setState({publications, publicationsLoaded: true});
       });
     });
@@ -131,6 +131,7 @@ class InteractionsEdgeTooltip extends React.Component {
       return this.renderEdge(selectedEdge, publications);
     } else {
       if( parallelEdges.length === 1 ){
+        this.selectEdge(parallelEdges[0]);
         return this.renderEdge(parallelEdges[0], []);
       } else {
         return this.renderEdgeChoice(parallelEdges);

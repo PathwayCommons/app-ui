a8b6422652d4b5fc46510b15bce347a82785e2de
Jeffrey Wong <jvwong@outlook.com>
2019-03-19 09:17:20 -0400

    Upgrading identifers.org and biopax suggester security.

--- src/config.js
+++ src/config.js
@@ -4,10 +4,10 @@ let defaults = {
   PORT: 3000,
   METADATA_CRON_SCHEDULE: '0 0 * * Monday', // update file from gprofiler etc. (Monday at midnight)
   PC_URL: 'https://www.pathwaycommons.org/',
-  XREF_SERVICE_URL: 'http://biopax.baderlab.org/',
+  XREF_SERVICE_URL: 'https://biopax.baderlab.org/',
   GPROFILER_URL: "https://biit.cs.ut.ee/gprofiler/",
   GMT_ARCHIVE_URL: 'https://biit.cs.ut.ee/gprofiler/static/gprofiler_hsapiens.name.zip',
-  IDENTIFIERS_URL: 'http://identifiers.org',
+  IDENTIFIERS_URL: 'https://identifiers.org',
   NCBI_EUTILS_BASE_URL: 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils',
   NCBI_API_KEY: 'b99e10ebe0f90d815a7a99f18403aab08008', // for dev testing only (baderlabsysmonitor ncbi key)
   HGNC_BASE_URL: 'https://rest.genenames.org',

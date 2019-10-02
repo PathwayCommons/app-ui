7f9ef5d1d26f0b60bcef6ef9e61eb445e01d65b2
jvwong <jvwong@gmail.com>
2019-10-01 21:18:23 -0400

    Added fetch archive utility

--- src/server/routes/enrichment/visualization/update-enrichment.js
+++ src/server/routes/enrichment/visualization/update-enrichment.js
@@ -1,9 +1,6 @@
-const request = require('request');
-const unzipper = require('unzipper');
-const sanitize = require("sanitize-filename");
 const logger = require('../../../logger');
+const { fetchArchiveFiles, sanitize } = require('../../../util');
 const { GMT_ARCHIVE_URL } = require('../../../../config.js');
-
 const { handleFileUpdate } = require('./pathway-table');
 
  const GMT_ARCHIVE_FILENAMES = [
@@ -12,14 +9,10 @@ const { handleFileUpdate } = require('./pathway-table');
 ];
 
 const SANITIZED_GMT_ARCHIVE_FILENAMES = GMT_ARCHIVE_FILENAMES.map( sanitize );
-const fetchZipCDFiles = url => unzipper.Open.url( request, url ).then( cd => cd.files );
-const pickFiles = files => files.filter( d => SANITIZED_GMT_ARCHIVE_FILENAMES.indexOf( sanitize( d.path ) ) > -1 );
 const updateGmt = url => {
-  fetchZipCDFiles( url )
-    .then( pickFiles )
+  fetchArchiveFiles( url, SANITIZED_GMT_ARCHIVE_FILENAMES )
     .then( handleFileUpdate )
-    .then( () => logger.info(`Enrichment: Updated GMT from ${url}`) )
-    .catch( error => logger.error(`Enrichment: Failed to update GMT from ${url} - ${error}`) );
+    .then( () => logger.info(`Enrichment: Updated GMT from ${url}`) );
 };
 
 const updateEnrichment = () => {

91b5e4b408e57da70a72a02e3d674e10770eaee2
jvwong <jvwong@gmail.com>
2019-10-04 14:46:10 -0400

    More generic handling of archive files

--- src/server/routes/enrichment/visualization/update-enrichment.js
+++ src/server/routes/enrichment/visualization/update-enrichment.js
@@ -1,16 +1,25 @@
+const fs = require('fs');
 const path = require('path');
+const concat = require('concat');
+const Promise = require('bluebird');
 
-const { writeArchiveFiles } = require('../../../source-files');
+const writeFile = Promise.promisify( fs.writeFile );
+const { writeArchiveFiles, sanitize } = require('../../../source-files');
 const { GMT_ARCHIVE_URL } = require('../../../../config.js');
 const { GMT_SOURCE_FILENAME } = require('../../../../config');
 
-const GMT_SOURCE_PATH = path.resolve(__dirname, GMT_SOURCE_FILENAME);
+const GMT_SOURCE_PATH = path.resolve(__dirname);
 
- const GMT_ARCHIVE_FILENAMES = [
+const GMT_ARCHIVE_FILENAMES = [
   'hsapiens.GO/BP.name.gmt',
   'hsapiens.REAC.name.gmt'
 ];
 
-const updateEnrichment = () => writeArchiveFiles( GMT_ARCHIVE_URL, GMT_ARCHIVE_FILENAMES, GMT_SOURCE_PATH );
+const updateEnrichment = () => {
+  return writeArchiveFiles( GMT_ARCHIVE_URL, GMT_ARCHIVE_FILENAMES, GMT_SOURCE_PATH )
+    .then( concat )
+    .then( data => writeFile( path.resolve( GMT_SOURCE_PATH, GMT_SOURCE_FILENAME ), data ) )
+    .then( () => true );
+}
 
 module.exports = { updateEnrichment };

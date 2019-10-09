c549df880838f5d45e59a4f143a6e422e57be0e6
jvwong <jvwong@gmail.com>
2019-10-09 17:34:59 -0400

    default to the downloads folder in source-files.js

--- src/server/routes/enrichment/visualization/update-enrichment.js
+++ src/server/routes/enrichment/visualization/update-enrichment.js
@@ -6,18 +6,17 @@ const Promise = require('bluebird');
 const writeFile = Promise.promisify( fs.writeFile );
 const { writeArchiveFiles } = require('../../../source-files');
 const { GMT_ARCHIVE_URL } = require('../../../../config.js');
-const { DOWNLOADS_FOLDER_NAME, GMT_SOURCE_FILENAME } = require('../../../../config');
+const { GMT_SOURCE_FILENAME } = require('../../../../config');
 
 const GMT_SOURCE_PATH = path.resolve(__dirname);
-const DOWNLOADS_FOLDER_PATH = path.resolve(__dirname, '../../../../../', DOWNLOADS_FOLDER_NAME);
 
 const GMT_ARCHIVE_FILENAMES = [
   'hsapiens.GO/BP.name.gmt',
   'hsapiens.REAC.name.gmt'
 ];
 
-const updateEnrichment = () => { console.log(DOWNLOADS_FOLDER_PATH);
-  return writeArchiveFiles( GMT_ARCHIVE_URL, GMT_ARCHIVE_FILENAMES, DOWNLOADS_FOLDER_PATH )
+const updateEnrichment = () => { 
+  return writeArchiveFiles( GMT_ARCHIVE_URL, GMT_ARCHIVE_FILENAMES )
     .then( concat )
     .then( data => writeFile( path.resolve( GMT_SOURCE_PATH, GMT_SOURCE_FILENAME ), data ) )
     .then( () => true );

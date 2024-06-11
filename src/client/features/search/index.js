8b0ad5953366e7afb204042f14332379cd2c3991
Jeffrey Wong <jvwong@gmail.com>
2024-06-11 16:23:41 -0400

    Point the example to adhesion related pathways (i.e. Biofactoid).

--- src/client/features/search/index.js
+++ src/client/features/search/index.js
@@ -203,8 +203,8 @@ class Search extends React.Component {
           h('div.search-suggestions', [
             'e.g. ',
             h(Link, { to: { pathname: '/search', search: queryString.stringify(_.assign({}, query, {q: 'cell cycle'})) }}, 'cell cycle, '),
-            h(Link, { to: { pathname: '/search', search: queryString.stringify(_.assign({}, query, {q: 'pcna xrcc2 xrcc3 rad50 rad51'})) }}, 'pcna xrcc2 xrcc3 rad50 rad51, '),
-            h(Link, { to: { pathname: '/search', search: queryString.stringify(_.assign({}, query, {q: 'P12004'})) }}, 'P12004')
+            h(Link, { to: { pathname: '/search', search: queryString.stringify(_.assign({}, query, {q: 'SRC TLN1 DLC1 PXN VCL KANK1'})) }}, 'SRC TLN1 DLC1 PXN VCL KANK1, '),
+            h(Link, { to: { pathname: '/search', search: queryString.stringify(_.assign({}, query, {q: 'uniprot:Q9Y490'})) }}, 'uniprot:Q9Y490')
           ])
         ])
       ]),

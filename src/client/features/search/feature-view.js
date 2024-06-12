eb54edc9e28e2a39785c2773afd91a85482a0972
Jeffrey Wong <jvwong@gmail.com>
2024-06-12 15:41:41 -0400

    Simplify the feature body and article paper link content.

--- src/client/features/search/feature-view.js
+++ src/client/features/search/feature-view.js
@@ -5,10 +5,7 @@ const _ = require('lodash');
 
 const {
   NS_BIOFACTOID,
-  NS_PATHWAYCOMMONS,
-  NS_NCBI_GENE,
-  NS_CHEBI,
-  FACTOID_URL
+  NS_PATHWAYCOMMONS
 } = require('../../../config');
 
 class FeatureView extends React.Component {
@@ -18,38 +15,14 @@ class FeatureView extends React.Component {
     const { feature } = this.props;
     if( feature == null ) return null;
 
-    const MAX_AUTHORS = 20;
+    const MAX_AUTHORS = 8;
 
-    const { article, pathways, entities, authors } = feature;
+    const { article, pathways, authors } = feature;
     const pcPathway = _.find( pathways, ['db', NS_PATHWAYCOMMONS] );
     const biofactoidPathway = _.find( pathways, ['db', NS_BIOFACTOID] );
 
     // Card Content
-    const pathwayTextComponent = biofactoidPathway.text.map( ({ title, body }) => {
-      return h( 'p', [
-        h('span.sub-heading', title),
-        h('span', body)
-      ]);
-    });
-
-    const entityComponent = h( 'p', [
-      h('span.sub-heading', 'Genes & Chemicals'),
-      h('ul.horizontal-list',
-        entities.map( ({ label, url, dbPrefix }) => {
-          let icon = null;
-          if ( dbPrefix.toLowerCase() === NS_NCBI_GENE.toLowerCase() ) {
-            icon = h('i.icon.icon-ncbi');
-          } else if ( dbPrefix.toLowerCase() === NS_CHEBI.toLowerCase() ) {
-            icon = h('i.icon.icon-chebi');
-          }
-          return h( 'li', [
-            h('a.plain-link', { href: url, target: '_blank' }, label ),
-            icon
-          ]);
-      }))
-    ]);
-
-    const cardBody = _.concat( entityComponent, pathwayTextComponent );
+    const body = biofactoidPathway.text;
 
     // Authors
     let authorList = authors.map( ({ url: href, label }, key) => {
@@ -76,32 +49,16 @@ class FeatureView extends React.Component {
 
     return (
       h('div.feature-container', [
-        h('div.feature-area.header', [
-          h('h3', [
-            h('span', 'Pathway curated by author (via '),
-            h('a.plain-link', {
-              href: FACTOID_URL,
-              target: '_blank'
-            }, [
-              h('span', 'biofactoid.org')
-            ]),
-            h('span', ')')
-          ])
-        ]),
         h('div.feature-area.pathway', [
           h('div.feature-item', [
             h(AppCard, {
               url: biofactoidPathway.url,
               image: h('img', { src: biofactoidPathway.imageSrc }),
               title: h('div', [
-                h('a', {
-                  href: biofactoidPathway.url,
-                  target: '_blank'
-                }, [ h('i.icon.logo-biofactoid'),
+                h('i.icon.logo-biofactoid'),
                 biofactoidPathway.organism ? h('span', biofactoidPathway.organism ) : null
-               ])
               ]),
-              body: h('div', cardBody )
+              body
             }),
             h('a.plain-link', {
               href: `/pathways?uri=${pcPathway.url}`,
@@ -113,16 +70,18 @@ class FeatureView extends React.Component {
           h('div.feature-item', [
             h('div.headline', article.title ),
             h('ul.horizontal-list.feature-detail', authorList ),
-            h('div.feature-item-row.feature-detail',
+            h('div.horizontal-list.feature-detail.feature-detail-links',
               [
-                h('div', [
+                h('div.feature-detail-link', [
                   article.doiUrl ?
                     h('a.plain-link', { href: article.doiUrl, target: '_blank' }, article.reference ) :
                     h( 'span', article.reference ),
                 ]),
-                article.pubmedUrl ? h('div', [
-                    h('a', { href: article.pubmedUrl, target: '_blank' }, h('i.icon.logo-pubmed') )
-                  ]) : null
+                h('div.feature-detail-link', [
+                  article.pubmedUrl ?
+                    h('a.plain-link', { href: article.pubmedUrl, target: '_blank' }, 'PubMed' ) :
+                    null
+                ])
               ]
             )
           ])
